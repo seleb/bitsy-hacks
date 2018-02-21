f9b37b3b80df3daa559ebe2a5dd4f05016790df0
mildmojo <mildmojo@users.noreply.github.com>
2018-02-21 01:52:37 -0500

    Converts {end} functions to (end) syntax.

--- end-from-dialog.js
+++ end-from-dialog.js
@@ -5,38 +5,39 @@
 
   Lets you end the game from dialog (including inside conditionals).
 
-  Using the {end} function in any part of a series of dialog will make the
+  Using the (end) function in any part of a series of dialog will make the
   game end after the dialog is finished. Ending the game resets it back to the
   intro.
 
-  Using {endNow} at the end of a sentence will display the whole sentence and
+  Using (endNow) at the end of a sentence will display the whole sentence and
   immediately clear the background. No further dialog from that passage will
-  display, and the game will reset when you proceed. Using {endNow} with
+  display, and the game will reset when you proceed. Using (endNow) with
   narration text will immediately exit the dialog, clear the background, and
   show the ending narration in an ending-style centered dialog box.
 
-  Usage: {end}
-         {end "<ending narration>"}
-         {endNow}
-         {endNow "<ending narration>"}
+  Usage: (end)
+         (end "<ending narration>")
+         (endNow)
+         (endNow "<ending narration>")
 
-  Example: {end}
-           {end "Five friars bid you goodbye. You leave the temple, hopeful."}
-           {endNow "The computer is still online! The chamber floods with neurotoxin."}
+  Example: (end)
+           (end "Five friars bid you goodbye. You leave the temple, hopeful.")
+           (endNow "The computer is still online! The chamber floods with neurotoxin.")
 
   HOW TO USE:
     1. Copy-paste this script into a new script tag after the Bitsy source code.
+       It should appear *before* any other mods that handle loading your game
+       data so it executes *after* them (last-in first-out).
 
-  NOTE: DON'T EDIT DIALOG FOR SPRITES/ITEMS WITH {end} CALLS IN THE DIALOG WINDOW.
-        Always edit them in the dialog textbox of the sprite/item paint window.
-        Editing any part of a sprite or item's dialog in the dialog window will
-        cause the editor replace that sprite/item's `{end}` with `{}` and you
-        probably won't notice that your endings are busted.
+  NOTE: This uses parentheses "()" instead of curly braces "{}" around function
+        calls because the Bitsy editor's fancy dialog window strips unrecognized
+        curly-brace functions from dialog text. To keep from losing data, write
+        these function calls with parentheses like the examples above.
 
         For full editor integration, you'd *probably* also need to paste this
         code at the end of the editor's `bitsy.js` file. Untested.
 
-  Version: 1.0
+  Version: 1.1
   Bitsy Version: 4.5, 4.6
   License: WTFPL (do WTF you want) except the `_inject` function by @seleb
 */
@@ -46,6 +47,17 @@
   'use strict';
 
   var queuedEndingNarration = null;
+
+  // Hook into game load and rewrite custom functions in game data to Bitsy format.
+  var _load_game = load_game;
+  globals.load_game = function(game_data, startWithTitle) {
+     // Rewrite custom functions' parentheses to curly braces for Bitsy's
+     // interpreter. Unescape escaped parentheticals, too.
+    var fixedGameData = game_data
+      .replace(/(^|[^\\])\(((end|endNow)( ".+?")?)\)/g, "$1{$2}") // Rewrite (end...) to {end...}
+      .replace(/(^|\\)\(((end|endNow)( ".+?")?)\\?\)/g, "($2)");   // Rewrite \(end...\) to (end...)
+    _load_game.call(this, fixedGameData, startWithTitle);
+  };
 
   // Hook into the game reset and make sure queued ending data gets cleared.
   var _clearGameData = clearGameData;
@@ -133,3 +145,4 @@
   };
 
 })(window);
+// End of (end) dialog function mod
