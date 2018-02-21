4d836d86e2238663f609426c7ae52232eecb0e1e
mildmojo <mildmojo@users.noreply.github.com>
2018-02-21 14:19:43 -0500

    Changes to favicon generator after code review.
    - Calculates all favicon animation frame data URLs up front
    - Creates the favicon <link> and sets properties once up front
    - Extracts some magic numbers to constants
    - Sets favicon virtual pixel size relative to Bitsy sprite size
      (i.e. Bitsy sprites are 8px and favicons are 16px, so one Bitsy
       pixel is 2x2 favicon pixels)
    - Moves animation loop to `startExportedGame` so it never has to stop
    
    PR #6

--- favicon-from-sprite.js
+++ favicon-from-sprite.js
@@ -21,7 +21,8 @@
 (function(globals) {
   'use strict';
 
-  // Configuration for favicon
+  // CONFIGURATION FOR FAVICON
+
   // Sprite name as configured in editor. Defaults to player avatar.
   var SPRITE_NAME = '';
   // Palette name or number to draw colors from.
@@ -37,82 +38,87 @@
   // Frame change interval if sprite is animated. Use `Infinity` to disable.
   var FRAME_DELAY = 400;
 
-  var canvas = document.createElement('canvas');
-  canvas.width = 16+2*PIXEL_PADDING;
-  canvas.height = 16+2*PIXEL_PADDING;
-  var ctx = canvas.getContext('2d');
-  var faviconLoop;
+  // END CONFIG
 
-  var _load_game = load_game;
-  globals.load_game = function(game_data, startWithTitle) {
-    _load_game.apply(this, arguments);
+  var FAVICON_SIZE = 16; // pixels
+  var ONE_PIXEL_SCALED = FAVICON_SIZE / tilesize;
+  PIXEL_PADDING *= ONE_PIXEL_SCALED;
+  var canvas = document.createElement('canvas');
+  canvas.width = FAVICON_SIZE + PIXEL_PADDING;
+  canvas.height = FAVICON_SIZE + PIXEL_PADDING;
+  var ctx = canvas.getContext('2d');
+  var faviconLinkElem;
+  var faviconFrameURLs = [];
+
+  var _startExportedGame = startExportedGame;
+  globals.startExportedGame = function() {
     startFaviconLoop();
+    _startExportedGame.apply(this, arguments);
   };
 
-  var _stopGame = stopGame;
-  globals.stopGame = function() {
-    _stopGame.apply(this, arguments);
-    stopFaviconLoop();
-  }
-
   function startFaviconLoop() {
-    var frameCount = getFrames(SPRITE_NAME).length;
-    if (frameCount === 1) {
-      return drawFavicon();
+    var frameNum = 0;
+    var frames = getFrames(SPRITE_NAME);
+
+    faviconFrameURLs = frames.map(drawFrame);
+
+    if (frames.length === 1) {
+      return updateBrowserFavicon(faviconFrameURLs[0]);
     }
 
-    var i = 0;
-    faviconLoop = setInterval(function() {
-      i = i % frameCount;
-      drawFavicon(i++);
+    setInterval(function() {
+      frameNum = frameNum % frameCount;
+      updateBrowserFavicon(faviconFrameURLs[frameNum]);
     }, FRAME_DELAY);
   }
 
-  function stopFaviconLoop() {
-    clearInterval(faviconLoop);
-  }
-
-  function drawFavicon(frameNum = 0) {
+  function drawFrame(frameData) {
     var pal = getPalette(PALETTE_ID);
     var bgColor = pal && pal[BG_COLOR_NUM] || [20,20,20];
     var spriteColor = pal && pal[FG_COLOR_NUM] || [245,245,245];
-    var frames = getFrames(SPRITE_NAME);
-    var frame = frames[frameNum] || frames[0];
-    var rounding_offset = ROUNDED_CORNERS ? 2 : 0;
+    var rounding_offset = ROUNDED_CORNERS ? ONE_PIXEL_SCALED : 0;
 
-    // Fill background color.
-    ctx.fillStyle = 'rgb(' + bgColor.join(',') + ')';
+    // Approximate a squircle-shaped background by drawing a fat plus sign with
+    // two overlapping rects, leaving some empty pixels in the corners.
+    var longSide = FAVICON_SIZE + PIXEL_PADDING;
+    var shortSide = longSide - rounding_offset*ONE_PIXEL_SCALED;
+    ctx.fillStyle = rgb(bgColor);
     ctx.fillRect(rounding_offset,
                  0,
-                 (16-2*rounding_offset)+2*PIXEL_PADDING,
-                 16+2*PIXEL_PADDING);
+                 shortSide,
+                 longSide);
     ctx.fillRect(0,
                  rounding_offset,
-                 16+2*PIXEL_PADDING,
-                 (16-2*rounding_offset)+2*PIXEL_PADDING);
+                 longSide,
+                 shortSide);
 
     // Draw sprite foreground.
-    ctx.fillStyle = 'rgb(' + spriteColor.join(',') + ')';
-    for (var y in frame) {
-      for (var x in frame[y]) {
-        if (frame[y][x] === 1) ctx.fillRect(x*2+PIXEL_PADDING,y*2+PIXEL_PADDING,2,2);
+    ctx.fillStyle = rgb(spriteColor);
+    for (var y in frameData) {
+      for (var x in frameData[y]) {
+        if (frameData[y][x] === 1) {
+          ctx.fillRect(x*ONE_PIXEL_SCALED + PIXEL_PADDING,
+                       y*ONE_PIXEL_SCALED + PIXEL_PADDING,
+                       ONE_PIXEL_SCALED,
+                       ONE_PIXEL_SCALED);
+        }
       }
     }
 
-    updateBrowserFavicon();
+    return canvas.toDataURL("image/x-icon");
   }
 
-  function updateBrowserFavicon() {
+  function updateBrowserFavicon(dataURL) {
     // Add or modify favicon link tag in document.
-    var link = document.querySelector('#favicon');
-    if (!link) {
-      link = document.createElement('link');
-      link.id = 'favicon';
-      document.getElementsByTagName('head')[0].appendChild(link);
+    faviconLinkElem = faviconLinkElem || document.querySelector('#favicon');
+    if (!faviconLinkElem) {
+      faviconLinkElem = document.createElement('link');
+      faviconLinkElem.id = 'favicon';
+      faviconLinkElem.type = 'image/x-icon';
+      faviconLinkElem.rel = 'shortcut icon';
+      document.head.appendChild(faviconLinkElem);
     }
-    link.type = 'image/x-icon';
-    link.rel = 'shortcut icon';
-    link.href = canvas.toDataURL("image/x-icon");
+    faviconLinkElem.href = dataURL;
   }
 
   function getFrames(name) {
@@ -139,5 +145,10 @@
     }
 
     return pal.colors;
+  }
+
+  // Expects values = [r, g, b]
+  function rgb(values) {
+    return 'rgb(' + values.join(',') + ')';
   }
 })(window);
