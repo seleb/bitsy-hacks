ac131d521b4d205bd78fa97fbf1698ee119c0d33
mildmojo <mildmojo@users.noreply.github.com>
2018-02-23 00:31:41 -0500

    Makes favicon sprite/palette name configs case-insensitive.
    Making the config case-insensitive should help reduce config mistakes
    when retyping names into config constants.
    
    Now iterates over `sprite` to find the sprite to draw as a favicon
    instead of using the `names.sprite` lookup object.

--- favicon-from-sprite.js
+++ favicon-from-sprite.js
@@ -8,9 +8,9 @@
 
   HOW TO USE:
     1. Copy-paste this script into a new script tag after the Bitsy source code.
-    2. Edit the top of the script to configure which sprite and colors it should
-       use for the favicon. By default, it will draw the player avatar sprite in
-       the first available palette's colors.
+    2. Edit the configuration below to set which sprite and colors this mod
+       should use for the favicon. By default, it will render the player avatar
+       sprite in the first available palette's colors.
 
   Version: 1.0
   Bitsy Version: 4.5, 4.6
@@ -23,9 +23,9 @@
 
   // CONFIGURATION FOR FAVICON
 
-  // Sprite name as configured in editor. Defaults to player avatar.
+  // Sprite name as entered in editor (not case-sensitive). Defaults to player avatar.
   var SPRITE_NAME = '';
-  // Palette name or number to draw colors from.
+  // Palette name or number to draw colors from. (Names not case-sensitive.)
   var PALETTE_ID = 0;
   // Favicon background color in palette. 0 = BG, 1 = Tile, 2 = Sprite.
   var BG_COLOR_NUM = 0;
@@ -35,7 +35,7 @@
   var PIXEL_PADDING = 1;
   // Should the favicon have rounded corners? (Suggest margin 2px if rounding.)
   var ROUNDED_CORNERS = true;
-  // Frame change interval if sprite is animated. Use `Infinity` to disable.
+  // Frame change interval (ms) if sprite is animated. Use `Infinity` to disable.
   var FRAME_DELAY = 400;
 
   // END CONFIG
@@ -54,7 +54,7 @@
   globals.startExportedGame = function() {
     _startExportedGame.apply(this, arguments);
     startFaviconLoop();
-  }
+  };
 
   function startFaviconLoop() {
     var frameNum = 0;
@@ -123,8 +123,11 @@
   }
 
   function getFrames(spriteName) {
-    var spriteId = names.sprite.get(spriteName) || playerId;
-    var spriteData = sprite[spriteId];
+    // `spriteName` is case insensitive to avoid Bitsydev headaches.
+    var spriteKey = Object.keys(sprite).find(function(key) {
+      return (sprite[key].name || '').toLowerCase() === spriteName.toLowerCase();
+    });
+    var spriteData = sprite[spriteKey || playerId];
     var frames = imageStore.source[spriteData.drw];
     return frames;
   }
@@ -134,8 +137,9 @@
 
     if (Number.isNaN(Number(palId))) {
       // Search palettes by name. `palette` is an object with numbers as keys. Yuck.
+      // Palette names are case-insensitive to avoid Bitsydev headaches.
       palId = Object.keys(palette).find(function(i) {
-        return palette[i].name === palId;
+        return (palette[i].name || '').toLowerCase() === palId.toLowerCase();
       });
     }
 
