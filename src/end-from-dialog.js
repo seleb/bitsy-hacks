19bf8035a62d58aaba3168ea19367d3601d83e9a
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-03-12 15:35:42 -0400

    refactored exit and end dialog hacks to use kitsy

--- src/end-from-dialog.js
+++ src/end-from-dialog.js
@@ -2,7 +2,7 @@
 🔚
 @file end-from-dialog
 @summary trigger an ending from dialog, including narration text
-@license WTFPL (do WTF you want) except the `_inject` function by @seleb
+@license WTFPL (do WTF you want)
 @version 1.1.0
 @requires Bitsy Version: 4.5, 4.6
 @author @mildmojo
@@ -42,108 +42,58 @@ NOTE: This uses parentheses "()" instead of curly braces "{}" around function
       For full editor integration, you'd *probably* also need to paste this
       code at the end of the editor's `bitsy.js` file. Untested.
 */
+'use strict';
+import bitsy from "bitsy";
+import {
+	kitsyInit
+} from "./kitsy-script-toolkit.js";
 
-// Give a hoot, don't pollute; encapsulate in an IIFE for isolation.
-(function(globals) {
-  'use strict';
+var kitsy = kitsyInit();
 
-  var queuedEndingNarration = null;
+var queuedEndingNarration = null;
 
-  // Hook into game load and rewrite custom functions in game data to Bitsy format.
-  var _load_game = load_game;
-  globals.load_game = function(game_data, startWithTitle) {
-     // Rewrite custom functions' parentheses to curly braces for Bitsy's
-     // interpreter. Unescape escaped parentheticals, too.
-    var fixedGameData = game_data
-      .replace(/(^|[^\\])\(((end|endNow)( ".+?")?)\)/g, "$1{$2}") // Rewrite (end...) to {end...}
-      .replace(/(^|\\)\(((end|endNow)( ".+?")?)\\?\)/g, "($2)");   // Rewrite \(end...\) to (end...)
-    _load_game.call(this, fixedGameData, startWithTitle);
-  };
+// Hook into game load and rewrite custom functions in game data to Bitsy format.
+kitsy.before('load_game', function (game_data, startWithTitle) {
+	// Rewrite custom functions' parentheses to curly braces for Bitsy's
+	// interpreter. Unescape escaped parentheticals, too.
+	var fixedGameData = game_data
+	.replace(/(^|[^\\])\(((end|endNow)( ".+?")?)\)/g, "$1{$2}") // Rewrite (end...) to {end...}
+	.replace(/(^|\\)\(((end|endNow)( ".+?")?)\\?\)/g, "($2)"); // Rewrite \(end...\) to (end...)
+	return [fixedGameData, startWithTitle];
+});
 
-  // Hook into the game reset and make sure queued ending data gets cleared.
-  var _clearGameData = clearGameData;
-  globals.clearGameData = function() {
-    _clearGameData.apply(this, arguments);
-    queuedEndingNarration = null;
-  };
+// Hook into the game reset and make sure queued ending data gets cleared.
+kitsy.after('clearGameData', function () {
+	queuedEndingNarration = null;
+});
 
-  // Hook into the dialog finish event; if there was an {end}, start the ending.
-  var _onExitDialog = onExitDialog;
-  globals.onExitDialog = function() {
-    _onExitDialog.apply(this, arguments);
-    if (!isEnding && queuedEndingNarration) {
-      startNarrating(queuedEndingNarration === true ? null : queuedEndingNarration, true);
-    }
-  };
+// Hook into the dialog finish event; if there was an {end}, start the ending.
+kitsy.after('onExitDialog', function () {
+	if (!bitsy.isEnding && queuedEndingNarration) {
+		bitsy.startNarrating(queuedEndingNarration === true ? null : queuedEndingNarration, true);
+	}
+});
 
-  // Implement the {end} dialog function. It stores the ending narration, if any,
-  // and schedules the game to end after the current dialog finishes.
-  globals.endFunc = function(environment, parameters, onReturn) {
-    queuedEndingNarration = parameters || true;
+// Implement the {end} dialog function. It stores the ending narration, if any,
+// and schedules the game to end after the current dialog finishes.
+bitsy.endFunc = function (environment, parameters, onReturn) {
+	queuedEndingNarration = parameters || true;
 
-    onReturn(null);
-  }
+	onReturn(null);
+}
 
-  // Implement the {endNow} dialog function. It starts ending narration, if any,
-  // and restarts the game right damn now.
-  globals.endNowFunc = function(environment, parameters, onReturn) {
-    endFunc.call(this, environment, parameters, function(){});
-    dialogBuffer.EndDialog();
-    onExitDialog();
-  }
+// Implement the {endNow} dialog function. It starts ending narration, if any,
+// and restarts the game right damn now.
+bitsy.endNowFunc = function (environment, parameters, onReturn) {
+	bitsy.endFunc.call(this, environment, parameters, function () {});
+	bitsy.dialogBuffer.EndDialog();
+	bitsy.onExitDialog();
+}
 
-  // Rewrite the Bitsy script tag, making these new functions callable from dialog.
-  _inject(
-    'var functionMap = new Map();',
-    'functionMap.set("end", endFunc);',
-    'functionMap.set("endNow", endNowFunc);'
-  );
-
-  // From https://gist.github.com/seleb/27798c1022e14aba82b9b77b97ad8002
-  // helper used to inject code into script tags based on a search string
-  function _inject(searchString, codeToInject) {
-    // find the relevant script tag
-    searchString = arguments[0];
-    codeToInject = [].slice.call(arguments, 1).join('');
-
-    var scriptTags = document.getElementsByTagName('script');
-    var scriptTag;
-    var code;
-    for (var i = 0; i < scriptTags.length; ++i) {
-      scriptTag = scriptTags[i];
-      if (
-        scriptTag.textContent.indexOf(searchString) >= 0 // script contains the search string
-        &&
-        scriptTag != document.currentScript // script isn't the one doing the injecting (which also contains the search string)
-      ) {
-        code = scriptTag.textContent;
-        break;
-      }
-    }
-
-    // error-handling
-    if (!code) {
-      throw 'Couldn\'t find "' + searchString + '" in script tags';
-    }
-
-    // modify the content
-    code = code.replace(searchString, searchString + codeToInject);
-
-    // replace the old script tag with a new one using our modified code
-    scriptTag.remove();
-    scriptTag = document.createElement('script');
-    scriptTag.textContent = code;
-    document.head.appendChild(scriptTag);
-
-    // recreate the script and dialog objects so that they'll be
-    // referencing the code with injections instead of the original
-    scriptModule = new Script();
-    scriptInterpreter = scriptModule.CreateInterpreter();
-
-    dialogModule = new Dialog();
-    dialogRenderer = dialogModule.CreateRenderer();
-    dialogBuffer = dialogModule.CreateBuffer();
-  };
-
-})(window);
+// Rewrite the Bitsy script tag, making these new functions callable from dialog.
+kitsy.inject(
+	'var functionMap = new Map();',
+	'functionMap.set("end", endFunc);',
+	'functionMap.set("endNow", endNowFunc);'
+);
 // End of (end) dialog function mod
