3ca387801414386d9f06fb1dbde067f69f32b548
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-03-13 12:30:39 -0400

    refactored favicon to use kitsy and hackOptions

--- src/favicon-from-sprite.js
+++ src/favicon-from-sprite.js
@@ -17,150 +17,134 @@ HOW TO USE:
      should use for the favicon. By default, it will render the player avatar
      sprite in the first available palette's colors.
 */
+'use strict';
+import bitsy from "bitsy";
+import {
+	kitsyInit
+} from "./kitsy-script-toolkit.js";
 
-// Give a hoot, don't pollute; encapsulate in an IIFE for isolation.
-(function(globals) {
-  'use strict';
+// CONFIGURATION FOR FAVICON
+var hackOptions = {
+	SPRITE_NAME: '', // Sprite name as entered in editor (not case-sensitive). Defaults to player avatar.
+	PALETTE_ID: 0, // Palette name or number to draw colors from. (Names not case-sensitive.)
+	BG_COLOR_NUM: 0, // Favicon background color in palette. 0 = BG, 1 = Tile, 2 = Sprite.
+	FG_COLOR_NUM: 2, // Favicon sprite color in palette. 0 = BG, 1 = Tile, 2 = Sprite.
+	PIXEL_PADDING: 1, // Padding around sprite, in Bitsy pixel units.
+	ROUNDED_CORNERS: true, // Should the favicon have rounded corners? (Suggest margin 2px if rounding.)
+	FRAME_DELAY: 400 // Frame change interval (ms) if sprite is animated. Use `Infinity` to disable.
+};
+// END CONFIG
 
-  // CONFIGURATION FOR FAVICON
+var FAVICON_SIZE = 16; // pixels
+var ONE_PIXEL_SCALED = FAVICON_SIZE / bitsy.tilesize;
+hackOptions.PIXEL_PADDING *= ONE_PIXEL_SCALED;
+var canvas = document.createElement('canvas');
+canvas.width = FAVICON_SIZE + 2 * hackOptions.PIXEL_PADDING;
+canvas.height = FAVICON_SIZE + 2 * hackOptions.PIXEL_PADDING;
+var ctx = canvas.getContext('2d');
+var faviconLinkElem;
+var faviconFrameURLs = [];
+var isStarted = false;
 
-  // Sprite name as entered in editor (not case-sensitive). Defaults to player avatar.
-  var SPRITE_NAME = '';
-  // Palette name or number to draw colors from. (Names not case-sensitive.)
-  var PALETTE_ID = 0;
-  // Favicon background color in palette. 0 = BG, 1 = Tile, 2 = Sprite.
-  var BG_COLOR_NUM = 0;
-  // Favicon sprite color in palette. 0 = BG, 1 = Tile, 2 = Sprite.
-  var FG_COLOR_NUM = 2;
-  // Padding around sprite, in Bitsy pixel units.
-  var PIXEL_PADDING = 1;
-  // Should the favicon have rounded corners? (Suggest margin 2px if rounding.)
-  var ROUNDED_CORNERS = true;
-  // Frame change interval (ms) if sprite is animated. Use `Infinity` to disable.
-  var FRAME_DELAY = 400;
+var kitsy = kitsyInit();
 
-  // END CONFIG
+kitsy.after('load_game', function () {
+	if (isStarted) {
+		return;
+	}
+	isStarted = true;
 
-  var FAVICON_SIZE = 16; // pixels
-  var ONE_PIXEL_SCALED = FAVICON_SIZE / tilesize;
-  PIXEL_PADDING *= ONE_PIXEL_SCALED;
-  var canvas = document.createElement('canvas');
-  canvas.width = FAVICON_SIZE + 2*PIXEL_PADDING;
-  canvas.height = FAVICON_SIZE + 2*PIXEL_PADDING;
-  var ctx = canvas.getContext('2d');
-  var faviconLinkElem;
-  var faviconFrameURLs = [];
-  var isStarted = false;
+	var frameNum = 0;
+	var frames = getFrames(hackOptions.SPRITE_NAME);
 
-  after('load_game', startFaviconLoop);
+	faviconFrameURLs = frames.map(drawFrame);
 
-  function startFaviconLoop() {
-    if (isStarted) {
-      return;
-    }
-    isStarted = true;
-    
-    var frameNum = 0;
-    var frames = getFrames(SPRITE_NAME);
+	// Only one frame? Don't even bother with the loop, just paint the icon once.
+	if (frames.length === 1) {
+		updateBrowserFavicon(faviconFrameURLs[0]);
+		return;
+	}
 
-    faviconFrameURLs = frames.map(drawFrame);
+	setInterval(function () {
+		frameNum = ++frameNum % frames.length;
+		updateBrowserFavicon(faviconFrameURLs[frameNum]);
+	}, hackOptions.FRAME_DELAY);
+});
 
-    // Only one frame? Don't even bother with the loop, just paint the icon once.
-    if (frames.length === 1) {
-      return updateBrowserFavicon(faviconFrameURLs[0]);
-    }
+function drawFrame(frameData) {
+	var pal = getPalette(hackOptions.PALETTE_ID);
+	var bgColor = pal && pal[hackOptions.BG_COLOR_NUM] || [20, 20, 20];
+	var spriteColor = pal && pal[hackOptions.FG_COLOR_NUM] || [245, 245, 245];
+	var rounding_offset = hackOptions.ROUNDED_CORNERS ? ONE_PIXEL_SCALED : 0;
 
-    setInterval(function() {
-      frameNum = ++frameNum % frames.length;
-      updateBrowserFavicon(faviconFrameURLs[frameNum]);
-    }, FRAME_DELAY);
-  }
+	// Approximate a squircle-shaped background by drawing a fat plus sign with
+	// two overlapping rects, leaving some empty pixels in the corners.
+	var longSide = FAVICON_SIZE + 2 * hackOptions.PIXEL_PADDING;
+	var shortSide = longSide - rounding_offset * ONE_PIXEL_SCALED;
+	ctx.fillStyle = rgb(bgColor);
+	ctx.fillRect(rounding_offset,
+		0,
+		shortSide,
+		longSide);
+	ctx.fillRect(0,
+		rounding_offset,
+		longSide,
+		shortSide);
 
-  function drawFrame(frameData) {
-    var pal = getPalette(PALETTE_ID);
-    var bgColor = pal && pal[BG_COLOR_NUM] || [20,20,20];
-    var spriteColor = pal && pal[FG_COLOR_NUM] || [245,245,245];
-    var rounding_offset = ROUNDED_CORNERS ? ONE_PIXEL_SCALED : 0;
+	// Draw sprite foreground.
+	ctx.fillStyle = rgb(spriteColor);
+	for (var y in frameData) {
+		for (var x in frameData[y]) {
+			if (frameData[y][x] === 1) {
+				ctx.fillRect(x * ONE_PIXEL_SCALED + hackOptions.PIXEL_PADDING,
+					y * ONE_PIXEL_SCALED + hackOptions.PIXEL_PADDING,
+					ONE_PIXEL_SCALED,
+					ONE_PIXEL_SCALED);
+			}
+		}
+	}
 
-    // Approximate a squircle-shaped background by drawing a fat plus sign with
-    // two overlapping rects, leaving some empty pixels in the corners.
-    var longSide = FAVICON_SIZE + 2*PIXEL_PADDING;
-    var shortSide = longSide - rounding_offset*ONE_PIXEL_SCALED;
-    ctx.fillStyle = rgb(bgColor);
-    ctx.fillRect(rounding_offset,
-                 0,
-                 shortSide,
-                 longSide);
-    ctx.fillRect(0,
-                 rounding_offset,
-                 longSide,
-                 shortSide);
+	return canvas.toDataURL("image/x-icon");
+}
 
-    // Draw sprite foreground.
-    ctx.fillStyle = rgb(spriteColor);
-    for (var y in frameData) {
-      for (var x in frameData[y]) {
-        if (frameData[y][x] === 1) {
-          ctx.fillRect(x*ONE_PIXEL_SCALED + PIXEL_PADDING,
-                       y*ONE_PIXEL_SCALED + PIXEL_PADDING,
-                       ONE_PIXEL_SCALED,
-                       ONE_PIXEL_SCALED);
-        }
-      }
-    }
+function updateBrowserFavicon(dataURL) {
+	// Add or modify favicon link tag in document.
+	faviconLinkElem = faviconLinkElem || document.querySelector('#favicon');
+	if (!faviconLinkElem) {
+		faviconLinkElem = document.createElement('link');
+		faviconLinkElem.id = 'favicon';
+		faviconLinkElem.type = 'image/x-icon';
+		faviconLinkElem.rel = 'shortcut icon';
+		document.head.appendChild(faviconLinkElem);
+	}
+	faviconLinkElem.href = dataURL;
+}
 
-    return canvas.toDataURL("image/x-icon");
-  }
+function getFrames(spriteName) {
+	// `spriteName` is case insensitive to avoid Bitsydev headaches.
+	var spriteKey = Object.keys(bitsy.sprite).find(function (key) {
+		return bitsy.sprite[key].name && bitsy.sprite[key].name.toLowerCase() === spriteName.toLowerCase();
+	});
+	var spriteData = bitsy.sprite[spriteKey || bitsy.playerId];
+	var frames = bitsy.imageStore.source[spriteData.drw];
+	return frames;
+}
 
-  function updateBrowserFavicon(dataURL) {
-    // Add or modify favicon link tag in document.
-    faviconLinkElem = faviconLinkElem || document.querySelector('#favicon');
-    if (!faviconLinkElem) {
-      faviconLinkElem = document.createElement('link');
-      faviconLinkElem.id = 'favicon';
-      faviconLinkElem.type = 'image/x-icon';
-      faviconLinkElem.rel = 'shortcut icon';
-      document.head.appendChild(faviconLinkElem);
-    }
-    faviconLinkElem.href = dataURL;
-  }
+function getPalette(id) {
+	var palId = id;
 
-  function getFrames(spriteName) {
-    // `spriteName` is case insensitive to avoid Bitsydev headaches.
-    var spriteKey = Object.keys(sprite).find(function(key) {
-      return sprite[key].name && sprite[key].name.toLowerCase() === spriteName.toLowerCase();
-    });
-    var spriteData = sprite[spriteKey || playerId];
-    var frames = imageStore.source[spriteData.drw];
-    return frames;
-  }
+	if (Number.isNaN(Number(palId))) {
+		// Search palettes by name. `palette` is an object with numbers as keys. Yuck.
+		// Palette names are case-insensitive to avoid Bitsydev headaches.
+		palId = Object.keys(bitsy.palette).find(function (i) {
+			return bitsy.palette[i].name && bitsy.palette[i].name.toLowerCase() === palId.toLowerCase();
+		});
+	}
 
-  function getPalette(id) {
-    var palId = id;
+	return bitsy.getPal(palId);
+}
 
-    if (Number.isNaN(Number(palId))) {
-      // Search palettes by name. `palette` is an object with numbers as keys. Yuck.
-      // Palette names are case-insensitive to avoid Bitsydev headaches.
-      palId = Object.keys(palette).find(function(i) {
-        return palette[i].name && palette[i].name.toLowerCase() === palId.toLowerCase();
-      });
-    }
-
-    return getPal(palId);
-  }
-
-  // Expects values = [r, g, b]
-  function rgb(values) {
-    return 'rgb(' + values.join(',') + ')';
-  }
-
-  function after(functionName, afterFn) {
-    var superFn = globals[functionName];
-
-    globals[functionName] = function() {
-      superFn.apply(this, arguments);
-      afterFn.apply(this, arguments);
-    };
-  }
-
-})(window);
+// Expects values = [r, g, b]
+function rgb(values) {
+	return 'rgb(' + values.join(',') + ')';
+}
