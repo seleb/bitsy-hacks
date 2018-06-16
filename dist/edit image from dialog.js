690d0bf9ed3bb01bbc78dd6e9ea1d10495b7738f
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-06-16 06:40:18 -0400

    run build

--- dist/edit image from dialog.js
+++ dist/edit image from dialog.js
@@ -141,7 +141,7 @@ function flatten(list) {
 @file kitsy-script-toolkit
 @summary makes it easier and cleaner to run code before and after Bitsy functions or to inject new code into Bitsy script tags
 @license WTFPL (do WTF you want)
-@version 2.1.1
+@version 2.1.2
 @requires Bitsy Version: 4.5, 4.6
 @author @mildmojo
 
@@ -298,8 +298,8 @@ function addDialogFunction(tag, fn) {
 		// Rewrite custom functions' parentheses to curly braces for Bitsy's
 		// interpreter. Unescape escaped parentheticals, too.
 		var fixedGameData = game_data
-		.replace(new RegExp("(^|[^\\\\])\\((" + tag + " \"?.+?\"?)\\)", "g"), "$1{$2}") // Rewrite (tag...) to {tag...}
-		.replace(new RegExp("\\\\\\((" + tag + " \"?.+\"?)\\\\?\\)", "g"), "($1)"); // Rewrite \(tag...\) to (tag...)
+		.replace(new RegExp('(^|[^\\\\])\\((' + tag + '\\s+(".+?"|.+?))\\)', 'g'), '$1{$2}') // Rewrite (tag "..."|...) to {tag "..."|...}
+		.replace(new RegExp('\\\\\\((' + tag + '\\s+(".+"|.+))\\\\?\\)', 'g'), '($1)'); // Rewrite \(tag "..."|...\) to (tag "..."|...)
 		return [fixedGameData, startWithTitle];
 	});
 
