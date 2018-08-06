48deeca8351547f72c40ea84e037ae2e10206534
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-06 00:08:43 -0400

    update gamepad input hack
    - use kitsy instead of basic overwrite (better compatibility)
    - update for bitsy 5.x (key handlers migrated to `bitsy.input`)

--- dist/gamepad input.js
+++ dist/gamepad input.js
@@ -3,7 +3,8 @@
 @file gamepad input
 @summary HTML5 gamepad support
 @license MIT
-@version 1.0.0
+@version 2.0.0
+@requires Bitsy Version: 5.1
 @author Sean S. LeBlanc
 
 @description
@@ -384,15 +385,195 @@ var gamepads = {
 
 var inputGamepads = gamepads;
 
+/**
+@file utils
+@summary miscellaneous bitsy utilities
+@author Sean S. LeBlanc
+*/
 
-
-var _startExportedGame = bitsy.startExportedGame;
-bitsy.startExportedGame = function () {
-	inputGamepads.init();
-	if (_startExportedGame) {
-		_startExportedGame();
+/*
+Helper used to replace code in a script tag based on a search regex
+To inject code without erasing original string, using capturing groups; e.g.
+	inject(/(some string)/,'injected before $1 injected after')
+*/
+function inject(searchRegex, replaceString) {
+	// find the relevant script tag
+	var scriptTags = document.getElementsByTagName('script');
+	var scriptTag;
+	var code;
+	for (var i = 0; i < scriptTags.length; ++i) {
+		scriptTag = scriptTags[i];
+		var matchesSearch = scriptTag.textContent.search(searchRegex) !== -1;
+		var isCurrentScript = scriptTag === document.currentScript;
+		if (matchesSearch && !isCurrentScript) {
+			code = scriptTag.textContent;
+			break;
+		}
 	}
-};
+
+	// error-handling
+	if (!code) {
+		throw 'Couldn\'t find "' + searchRegex + '" in script tags';
+	}
+
+	// modify the content
+	code = code.replace(searchRegex, replaceString);
+
+	// replace the old script tag with a new one using our modified code
+	var newScriptTag = document.createElement('script');
+	newScriptTag.textContent = code;
+	scriptTag.insertAdjacentElement('afterend', newScriptTag);
+	scriptTag.remove();
+}
+
+/**
+ * Helper for getting an array with unique elements 
+ * @param  {Array} array Original array
+ * @return {Array}       Copy of array, excluding duplicates
+ */
+function unique(array) {
+	return array.filter(function (item, idx) {
+		return array.indexOf(item) === idx;
+	});
+}
+
+/**
+
+@file kitsy-script-toolkit
+@summary makes it easier and cleaner to run code before and after Bitsy functions or to inject new code into Bitsy script tags
+@license WTFPL (do WTF you want)
+@version 3.2.0
+@requires Bitsy Version: 4.5, 4.6
+@author @mildmojo
+
+@description
+HOW TO USE:
+  import {before, after, inject, addDialogTag, addDeferredDialogTag} from "./helpers/kitsy-script-toolkit";
+
+  before(targetFuncName, beforeFn);
+  after(targetFuncName, afterFn);
+  inject(searchRegex, replaceString);
+  addDialogTag(tagName, dialogFn);
+  addDeferredDialogTag(tagName, dialogFn);
+
+  For more info, see the documentation at:
+  https://github.com/seleb/bitsy-hacks/wiki/Coding-with-kitsy
+*/
+
+// Ex: before('load_game', function run() { alert('Loading!'); });
+//     before('show_text', function run(text) { return text.toUpperCase(); });
+//     before('show_text', function run(text, done) { done(text.toUpperCase()); });
+function before(targetFuncName, beforeFn) {
+	var kitsy = kitsyInit();
+	kitsy.queuedBeforeScripts[targetFuncName] = kitsy.queuedBeforeScripts[targetFuncName] || [];
+	kitsy.queuedBeforeScripts[targetFuncName].push(beforeFn);
+}
+
+// Ex: after('load_game', function run() { alert('Loaded!'); });
+function after(targetFuncName, afterFn) {
+	var kitsy = kitsyInit();
+	kitsy.queuedAfterScripts[targetFuncName] = kitsy.queuedAfterScripts[targetFuncName] || [];
+	kitsy.queuedAfterScripts[targetFuncName].push(afterFn);
+}
+
+function kitsyInit() {
+	// return already-initialized kitsy
+	if (bitsy.kitsy) {
+		return bitsy.kitsy;
+	}
+
+	// Initialize kitsy
+	bitsy.kitsy = {
+		queuedInjectScripts: [],
+		queuedBeforeScripts: {},
+		queuedAfterScripts: {}
+	};
+
+	var oldStartFunc = bitsy.startExportedGame;
+	bitsy.startExportedGame = function doAllInjections() {
+		// Only do this once.
+		bitsy.startExportedGame = oldStartFunc;
+
+		// Rewrite scripts and hook everything up.
+		doInjects();
+		applyAllHooks();
+
+		// Start the game
+		bitsy.startExportedGame.apply(this, arguments);
+	};
+
+	return bitsy.kitsy;
+}
+
+
+function doInjects() {
+	bitsy.kitsy.queuedInjectScripts.forEach(function (injectScript) {
+		inject(injectScript.searchRegex, injectScript.replaceString);
+	});
+	_reinitEngine();
+}
+
+function applyAllHooks() {
+	var allHooks = unique(Object.keys(bitsy.kitsy.queuedBeforeScripts).concat(Object.keys(bitsy.kitsy.queuedAfterScripts)));
+	allHooks.forEach(applyHook);
+}
+
+function applyHook(functionName) {
+	var superFn = bitsy[functionName];
+	var superFnLength = superFn.length;
+	var functions = [];
+	// start with befores
+	functions = functions.concat(bitsy.kitsy.queuedBeforeScripts[functionName] || []);
+	// then original
+	functions.push(superFn);
+	// then afters
+	functions = functions.concat(bitsy.kitsy.queuedAfterScripts[functionName] || []);
+
+	// overwrite original with one which will call each in order
+	bitsy[functionName] = function () {
+		var args = [].slice.call(arguments);
+		var i = 0;
+		runBefore.apply(this, arguments);
+
+		// Iterate thru sync & async functions. Run each, finally run original.
+		function runBefore() {
+			// All outta functions? Finish
+			if (i === functions.length) {
+				return;
+			}
+
+			// Update args if provided.
+			if (arguments.length > 0) {
+				args = [].slice.call(arguments);
+			}
+
+			if (functions[i].length > superFnLength) {
+				// Assume funcs that accept more args than the original are
+				// async and accept a callback as an additional argument.
+				functions[i++].apply(this, args.concat(runBefore.bind(this)));
+			} else {
+				// run synchronously
+				var newArgs = functions[i++].apply(this, args) || args;
+				runBefore.apply(this, newArgs);
+			}
+		}
+	};
+}
+
+function _reinitEngine() {
+	// recreate the script and dialog objects so that they'll be
+	// referencing the code with injections instead of the original
+	bitsy.scriptModule = new bitsy.Script();
+	bitsy.scriptInterpreter = bitsy.scriptModule.CreateInterpreter();
+
+	bitsy.dialogModule = new bitsy.Dialog();
+	bitsy.dialogRenderer = bitsy.dialogModule.CreateRenderer();
+	bitsy.dialogBuffer = bitsy.dialogModule.CreateBuffer();
+}
+
+
+
+before('startExportedGame', inputGamepads.init.bind(inputGamepads));
 var empty = function () {};
 
 var move = function (dpad, face, axis, axis2, axispast, axisdir, key) {
@@ -410,7 +591,7 @@ var move = function (dpad, face, axis, axis2, axispast, axisdir, key) {
 		)
 	) {
 		bitsy.curPlayerDirection = bitsy.Direction.None;
-		bitsy.onkeydown({
+		bitsy.input.onkeydown({
 			keyCode: key,
 			preventDefault: empty
 		});
@@ -422,24 +603,21 @@ var move = function (dpad, face, axis, axis2, axispast, axisdir, key) {
 		inputGamepads.isJustUp(face) ||
 		inputGamepads.axisJustPast(axis, axispast, -axisdir)
 	) {
-		bitsy.onkeyup({
+		bitsy.input.onkeyup({
 			keyCode: key,
 			preventDefault: empty
 		});
 	}
 };
 
-var _update = bitsy.update;
-bitsy.update = function () {
+before('update', function(){
 	move(inputGamepads.DPAD_LEFT, inputGamepads.X, inputGamepads.LSTICK_H, inputGamepads.RSTICK_H, -0.5, -1, bitsy.key.left);
 	move(inputGamepads.DPAD_RIGHT, inputGamepads.B, inputGamepads.LSTICK_H, inputGamepads.RSTICK_H, 0.5, 1, bitsy.key.right);
 	move(inputGamepads.DPAD_UP, inputGamepads.Y, inputGamepads.LSTICK_V, inputGamepads.RSTICK_V, -0.5, -1, bitsy.key.up);
 	move(inputGamepads.DPAD_DOWN, inputGamepads.A, inputGamepads.LSTICK_V, inputGamepads.RSTICK_V, 0.5, 1, bitsy.key.down);
-
-	if (_update) {
-		_update();
-	}
+});
+after('update', function(){
 	inputGamepads.update();
-};
+});
 
 }(window));
