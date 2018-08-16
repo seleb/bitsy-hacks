6a5f5d643e693eb01f8bbaa07d8d8b33f1fe28c4
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-15 22:36:08 -0400

    run build

--- dist/solid items.js
+++ dist/solid items.js
@@ -3,7 +3,7 @@
 @file solid items
 @summary treat some items like sprites that can be placed multiple times
 @license MIT
-@version 1.0.1
+@version 2.0.0
 @author Sean S. LeBlanc
 
 @description
@@ -27,54 +27,237 @@ var hackOptions = {
 
 bitsy = bitsy && bitsy.hasOwnProperty('default') ? bitsy['default'] : bitsy;
 
+/**
+@file utils
+@summary miscellaneous bitsy utilities
+@author Sean S. LeBlanc
+*/
 
-
-
-
-// true if item should be treated as sprite
-// false if item should be treated normally
-function getSolidItemFromIndex(itemIndex) {
-	if (itemIndex === -1) {
-		return;
+/*
+Helper used to replace code in a script tag based on a search regex
+To inject code without erasing original string, using capturing groups; e.g.
+	inject(/(some string)/,'injected before $1 injected after')
+*/
+function inject(searchRegex, replaceString) {
+	// find the relevant script tag
+	var scriptTags = document.getElementsByTagName('script');
+	var scriptTag;
+	var code;
+	for (var i = 0; i < scriptTags.length; ++i) {
+		scriptTag = scriptTags[i];
+		var matchesSearch = scriptTag.textContent.search(searchRegex) !== -1;
+		var isCurrentScript = scriptTag === document.currentScript;
+		if (matchesSearch && !isCurrentScript) {
+			code = scriptTag.textContent;
+			break;
+		}
 	}
-	var itemId = bitsy.room[bitsy.curRoom].items[itemIndex].id;
-	var item = bitsy.item[itemId];
-	if (hackOptions.itemIsSolid(item)) {
-		return item;
+
+	// error-handling
+	if (!code) {
+		throw 'Couldn\'t find "' + searchRegex + '" in script tags';
 	}
-	return;
+
+	// modify the content
+	code = code.replace(searchRegex, replaceString);
+
+	// replace the old script tag with a new one using our modified code
+	var newScriptTag = document.createElement('script');
+	newScriptTag.textContent = code;
+	scriptTag.insertAdjacentElement('afterend', newScriptTag);
+	scriptTag.remove();
 }
 
-var _getItemIndex = bitsy.getItemIndex;
-bitsy.getItemIndex = function () {
-	var itemIndex = _getItemIndex.apply(this, arguments);
-	var sprItem = getSolidItemFromIndex(itemIndex);
-	if (sprItem) {
-		return -1;
-	}
-	return itemIndex;
-};
+/**
+ * Helper for getting an array with unique elements 
+ * @param  {Array} array Original array
+ * @return {Array}       Copy of array, excluding duplicates
+ */
+function unique(array) {
+	return array.filter(function (item, idx) {
+		return array.indexOf(item) === idx;
+	});
+}
 
-var _getSpriteAt = bitsy.getSpriteAt;
-bitsy.getSpriteAt = function (x, y) {
-	var spr = _getSpriteAt.apply(this, arguments);
-	if (spr) {
-		return spr;
-	}
-	var itemIndex = _getItemIndex(bitsy.curRoom, x, y);
-	var item = getSolidItemFromIndex(itemIndex);
-	if (item) {
-		return item.drw;
-	}
-};
+/**
 
-var _startSpriteDialog = bitsy.startSpriteDialog;
-bitsy.startSpriteDialog = function (spriteId) {
-	var item = spriteId.split("ITM_")[1];
-	if (item) {
-		return bitsy.startItemDialog(item);
+@file kitsy-script-toolkit
+@summary makes it easier and cleaner to run code before and after Bitsy functions or to inject new code into Bitsy script tags
+@license WTFPL (do WTF you want)
+@version 3.2.1
+@requires Bitsy Version: 4.5, 4.6
+@author @mildmojo
+
+@description
+HOW TO USE:
+  import {before, after, inject, addDialogTag, addDeferredDialogTag} from "./helpers/kitsy-script-toolkit";
+
+  before(targetFuncName, beforeFn);
+  after(targetFuncName, afterFn);
+  inject(searchRegex, replaceString);
+  addDialogTag(tagName, dialogFn);
+  addDeferredDialogTag(tagName, dialogFn);
+
+  For more info, see the documentation at:
+  https://github.com/seleb/bitsy-hacks/wiki/Coding-with-kitsy
+*/
+
+// Ex: before('load_game', function run() { alert('Loading!'); });
+//     before('show_text', function run(text) { return text.toUpperCase(); });
+//     before('show_text', function run(text, done) { done(text.toUpperCase()); });
+function before(targetFuncName, beforeFn) {
+	var kitsy = kitsyInit();
+	kitsy.queuedBeforeScripts[targetFuncName] = kitsy.queuedBeforeScripts[targetFuncName] || [];
+	kitsy.queuedBeforeScripts[targetFuncName].push(beforeFn);
+}
+
+// Ex: after('load_game', function run() { alert('Loaded!'); });
+function after(targetFuncName, afterFn) {
+	var kitsy = kitsyInit();
+	kitsy.queuedAfterScripts[targetFuncName] = kitsy.queuedAfterScripts[targetFuncName] || [];
+	kitsy.queuedAfterScripts[targetFuncName].push(afterFn);
+}
+
+function kitsyInit() {
+	// return already-initialized kitsy
+	if (bitsy.kitsy) {
+		return bitsy.kitsy;
 	}
-	_startSpriteDialog.apply(this, arguments);
-};
+
+	// Initialize kitsy
+	bitsy.kitsy = {
+		queuedInjectScripts: [],
+		queuedBeforeScripts: {},
+		queuedAfterScripts: {}
+	};
+
+	var oldStartFunc = bitsy.startExportedGame;
+	bitsy.startExportedGame = function doAllInjections() {
+		// Only do this once.
+		bitsy.startExportedGame = oldStartFunc;
+
+		// Rewrite scripts and hook everything up.
+		doInjects();
+		applyAllHooks();
+
+		// Start the game
+		bitsy.startExportedGame.apply(this, arguments);
+	};
+
+	return bitsy.kitsy;
+}
+
+
+function doInjects() {
+	bitsy.kitsy.queuedInjectScripts.forEach(function (injectScript) {
+		inject(injectScript.searchRegex, injectScript.replaceString);
+	});
+	_reinitEngine();
+}
+
+function applyAllHooks() {
+	var allHooks = unique(Object.keys(bitsy.kitsy.queuedBeforeScripts).concat(Object.keys(bitsy.kitsy.queuedAfterScripts)));
+	allHooks.forEach(applyHook);
+}
+
+function applyHook(functionName) {
+	var superFn = bitsy[functionName];
+	var superFnLength = superFn ? superFn.length : 0;
+	var functions = [];
+	// start with befores
+	functions = functions.concat(bitsy.kitsy.queuedBeforeScripts[functionName] || []);
+	// then original
+	if (superFn) {
+		functions.push(superFn);
+	}
+	// then afters
+	functions = functions.concat(bitsy.kitsy.queuedAfterScripts[functionName] || []);
+
+	// overwrite original with one which will call each in order
+	bitsy[functionName] = function () {
+		var args = [].slice.call(arguments);
+		var i = 0;
+		runBefore.apply(this, arguments);
+
+		// Iterate thru sync & async functions. Run each, finally run original.
+		function runBefore() {
+			// All outta functions? Finish
+			if (i === functions.length) {
+				return;
+			}
+
+			// Update args if provided.
+			if (arguments.length > 0) {
+				args = [].slice.call(arguments);
+			}
+
+			if (functions[i].length > superFnLength) {
+				// Assume funcs that accept more args than the original are
+				// async and accept a callback as an additional argument.
+				functions[i++].apply(this, args.concat(runBefore.bind(this)));
+			} else {
+				// run synchronously
+				var newArgs = functions[i++].apply(this, args) || args;
+				runBefore.apply(this, newArgs);
+			}
+		}
+	};
+}
+
+function _reinitEngine() {
+	// recreate the script and dialog objects so that they'll be
+	// referencing the code with injections instead of the original
+	bitsy.scriptModule = new bitsy.Script();
+	bitsy.scriptInterpreter = bitsy.scriptModule.CreateInterpreter();
+
+	bitsy.dialogModule = new bitsy.Dialog();
+	bitsy.dialogRenderer = bitsy.dialogModule.CreateRenderer();
+	bitsy.dialogBuffer = bitsy.dialogModule.CreateBuffer();
+}
+
+
+
+
+
+var room;
+var oldItems;
+var x,y;
+before("movePlayer", function () {
+	x = bitsy.player().x;
+	y = bitsy.player().y;
+	room = bitsy.room[bitsy.curRoom];
+	oldItems = room.items.slice();
+});
+after("movePlayer", function () {
+	var newItems = room.items;
+	if (newItems.length === oldItems.length) {
+		return; // nothing changed
+	}
+
+	// check for changes
+	for (var i = 0; i < oldItems.length; ++i) {
+		if (!newItems[i] ||
+			oldItems[i].x !== newItems[i].x ||
+			oldItems[i].y !== newItems[i].y ||
+			oldItems[i].id !== newItems[i].id
+		) {
+			// something changed
+			if (hackOptions.itemIsSolid(bitsy.item[oldItems[i].id])) {
+				// put that back!
+				newItems.splice(i, 0, oldItems[i]);
+				// get back there!
+				bitsy.player().x = x;
+				bitsy.player().y = y;
+			} else {
+				// add an empty entry for now to keep the arrays aligned
+				newItems.splice(i, 0, null);
+			}
+		}
+	}
+	// clear out those empty entries
+	room.items = newItems.filter(function (item) {
+		return !!item;
+	});
+});
 
 }(window));
