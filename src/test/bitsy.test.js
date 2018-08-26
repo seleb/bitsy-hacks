1e2b87e882e45b2d2a7f5ccfb0984b1e8acce78e
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-25 20:13:24 -0400

    test the helpers

---
+++ src/test/bitsy.test.js
@@ -0,0 +1,33 @@
+// baseline test to make sure that bitsy helpers
+// function as expected
+import {
+	start,
+	walkToCat,
+	snapshot,
+	startRecording,
+	stopRecording,
+	end
+} from './bitsy';
+
+test('walkToCat moves the player to the left of the cat in the default game', async () => {
+	await start();
+	await walkToCat();
+	await snapshot();
+	await end();
+});
+
+test('startRecording starts capturing a snapshot after every key press', async () => {
+	await start();
+	await startRecording();
+	await walkToCat();
+	await end();
+});
+
+test('stopRecording stops capturing a snapshot after every key press', async () => {
+	await start();
+	await startRecording();
+	await stopRecording();
+	await walkToCat();
+	await snapshot();
+	await end();
+});
