fc889c39239fbd6de1f382d3b532980658ba8863
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-27 00:40:05 -0400

    add shader programmatically

--- src/transitions.js
+++ src/transitions.js
@@ -23,3 +23,22 @@ hackOptions.update = function() {
 	}
 };
 
+var shader = document.createElement("script");
+
+shader.id = "shader-frag";
+shader.type = "x-shader/x-fragment";
+shader.textContent = `
+	precision mediump float;
+	uniform sampler2D tex0;
+	uniform sampler2D tex1;
+	uniform float time;
+	uniform vec2 resolution;
+
+	void main(){
+		vec2 coord = gl_FragCoord.xy;
+		vec2 uv = coord.xy / resolution.xy;
+		vec3 col = texture2D(tex0,uv).rgb;
+		gl_FragColor = vec4(col, 1.0);
+	}
+`;
+document.head.appendChild(shader);
