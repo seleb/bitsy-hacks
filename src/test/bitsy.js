a6379d0e36693efc898e4dd7eaabc102880e4ab0
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-09-09 17:22:47 -0400

    update test version

--- src/test/bitsy.js
+++ src/test/bitsy.js
@@ -33,7 +33,7 @@ async function getHackDist() {
 }
 
 async function getHackTemplate() {
-	return readFile(resolve(__dirname, 'bitsy 5.1.html'), {
+	return readFile(resolve(__dirname, 'bitsy 5.3.html'), {
 		encoding: 'utf8'
 	});
 }
