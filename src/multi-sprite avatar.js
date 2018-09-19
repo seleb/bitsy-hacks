b17b97b7c12a9ecf7c36a969f6de790709c15ebc
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-09-19 03:16:28 -0400

    do hooks for multi sprite avatar in an `after` instead of at top-level

--- src/multi-sprite avatar.js
+++ src/multi-sprite avatar.js
@@ -160,13 +160,6 @@ var repeats = [
 	'isWallUp',
 	'isWallDown'
 ];
-for (var i = 0; i < repeats.length; ++i) {
-	var r = repeats[i];
-	var _fn = bitsy[r];
-	bitsy[r] = function (fn) {
-		return enabled ? repeat(fn) : fn();
-	}.bind(undefined, _fn);
-}
 
 // prevent player from colliding with their own pieces
 function filterPieces(id) {
@@ -177,7 +170,17 @@ function filterPieces(id) {
 	}
 	return id;
 }
-var _getSpriteAt = bitsy.getSpriteAt;
-bitsy.getSpriteAt = function () {
-	return filterPieces(_getSpriteAt.apply(this, arguments));
-}
+
+after('startExportedGame', function () {
+	for (var i = 0; i < repeats.length; ++i) {
+		var r = repeats[i];
+		var _fn = bitsy[r];
+		bitsy[r] = function (fn) {
+			return enabled ? repeat(fn) : fn();
+		}.bind(undefined, _fn);
+	}
+	var _getSpriteAt = bitsy.getSpriteAt;
+	bitsy.getSpriteAt = function () {
+		return filterPieces(_getSpriteAt.apply(this, arguments));
+	}
+});
