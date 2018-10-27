756b6402903f7b6411934c78eb179ae9254d9054
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-10-27 15:07:49 -0400

    test transparent sprites compatability

--- src/opaque tiles.test.js
+++ src/opaque tiles.test.js
@@ -17,3 +17,16 @@ test('opaque tiles', async () => {
 	await snapshot();
 	await end();
 });
+
+test('compatible with transparency', async () => {
+	await start({
+		hacks: ['opaque tiles', 'transparent sprites'],
+	});
+	await press('Enter'); // complete title page
+	await press('Enter'); // end title page
+	await press('ArrowLeft');
+	await press('ArrowLeft');
+	await press('ArrowLeft'); // walk on top of tile border
+	await snapshot();
+	await end();
+})
