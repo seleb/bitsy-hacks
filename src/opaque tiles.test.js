4e973bf1cb90b4da8ce0e7aa99f0c5184abf46d3
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-10-27 15:05:31 -0400

    test opaque tiles hack

---
+++ src/opaque tiles.test.js
@@ -0,0 +1,19 @@
+import {
+	start,
+	press,
+	end,
+	snapshot,
+} from './test/bitsy';
+
+test('opaque tiles', async () => {
+	await start({
+		hacks: ['opaque tiles'],
+	});
+	await press('Enter'); // complete title page
+	await press('Enter'); // end title page
+	await press('ArrowLeft');
+	await press('ArrowLeft');
+	await press('ArrowLeft'); // walk on top of tile border
+	await snapshot();
+	await end();
+});
