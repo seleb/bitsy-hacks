90b34a4654be58726f212389e41544c6232fdfef
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-11-05 02:51:59 -0500

    Direction in dialog + expose variables (#53)
    * add expose variables hack
    
    this is a really tiny one, but worth splitting into its own file
    
    * add direction in dialog hack
    
    * build
    
    * test direction in dialog hack

---
+++ src/direction in dialog.js
@@ -0,0 +1,44 @@
+/**
+🔝
+@file direction in dialog
+@summary provides a variable with player direction
+@license MIT
+@version 1.0.0
+@requires 5.3
+@author Sean S. LeBlanc
+
+@description
+Provides a variable "playerDirection" that can be accessed in dialog
+The value will be one of:
+	- "up"
+	- "down"
+	- "left"
+	- "right"
+Depending on the last input from the player.
+
+Note that the variable will describe the direction the player moved,
+so if they're interacting with a sprite, the opposite will be the direction from which they came
+i.e. if the player moves into a sprite from the left, the variable will be "right"
+
+HOW TO USE:
+Copy-paste into a script tag after the bitsy source
+*/
+import bitsy from "bitsy";
+import {
+	before,
+} from "./helpers/kitsy-script-toolkit";
+import "./expose variables";
+
+var keys = {};
+keys[bitsy.Direction.Up] = "up";
+keys[bitsy.Direction.Down] = "down";
+keys[bitsy.Direction.Left] = "left";
+keys[bitsy.Direction.Right] = "right";
+keys[bitsy.Direction.None] = null;
+
+before('startDialog', function () {
+	var direction = keys[bitsy.curPlayerDirection];
+	if (direction) {
+		bitsy.kitsy.variableMap.set('playerDirection', direction);
+	}
+});
---
+++ src/direction in dialog.test.js
@@ -0,0 +1,53 @@
+import {
+	start,
+	walkToCat,
+	press,
+	end,
+	snapshot,
+} from './test/bitsy';
+
+test('direction in dialog', async () => {
+	await start({
+		hacks: ['direction in dialog'],
+		catDialog: `"""
+{
+  - playerDirection == "up" ?
+    i'm a cat above you
+  - playerDirection == "left" ?
+    i'm a cat to the left of you
+  - playerDirection == "right" ?
+    i'm a cat to the right of you
+  - playerDirection == "down" ?
+    i'm a cat below you
+  - else ?
+    i'm a cat but the hack didn't work
+}
+"""`,
+	});
+	await walkToCat();
+	await press('ArrowRight'); // talk to cat from the right
+	await press('Enter'); // complete dialog page
+	await snapshot();
+	await press('Enter'); // close dialog page
+
+	await press('ArrowUp');
+	await press('ArrowRight');
+	await press('ArrowDown'); // talk to cat from above
+	await press('Enter'); // complete dialog page
+	await snapshot();
+	await press('Enter'); // close dialog page
+
+	await press('ArrowRight');
+	await press('ArrowDown');
+	await press('ArrowLeft'); // talk to cat from the left
+	await press('Enter'); // complete dialog page
+	await snapshot();
+	await press('Enter'); // close dialog page
+
+	await press('ArrowDown');
+	await press('ArrowLeft');
+	await press('ArrowUp'); // talk to cat from below
+	await press('Enter'); // complete dialog page
+	await snapshot();
+	await end();
+});
