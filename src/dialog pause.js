df5e196c972282d96299e9f5dd865f02d3d22dea
Sean LeBlanc <sean.s.leblanc@gmail.com>
2019-01-09 23:46:41 -0500

    add dialog pause hack (#58)
    * Create dialog pause.js
    
    * run build

---
+++ src/dialog pause.js
@@ -0,0 +1,26 @@
+/**
+💬
+@file dialog pause
+@summary add pauses in between printing text
+@license MIT
+@version 1.0.0
+@author Sean S. LeBlanc
+
+@description
+Adds a command that allows you to add pauses in between printing text.
+
+Example: (pause "1000")
+
+Note: pause times are in milliseconds
+
+HOW TO USE:
+Copy-paste into a script tag after the bitsy source
+*/
+import {
+	addDialogTag
+} from "./helpers/kitsy-script-toolkit";
+
+addDialogTag('pause', function (environment, parameters, onReturn) {
+	environment.GetDialogBuffer().Update(-parseFloat(parameters[0]));
+	onReturn(null);
+});
