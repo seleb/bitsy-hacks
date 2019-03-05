43d1fd38ef7d5dbdbb898260a15bb81e99e4c684
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-03-05 03:17:24 -0500

    update canvas replacement to use new webglazy api

--- src/canvas replacement.js
+++ src/canvas replacement.js
@@ -3,7 +3,7 @@
 @file canvas replacement
 @summary WebGLazy bitsy integration (this one's mostly just for me)
 @license MIT
-@version 2.1.0
+@version 3.0.0
 @author Sean S. LeBlanc
 
 @description
@@ -14,26 +14,29 @@ HOW TO USE:
 2. For finer scaling, edit `var text_scale = 2` and `var scale = 4` in the bitsy source to `var text_scale = 1` and `var scale = 2`
 3. Edit the hackOptions object passed to the `new WebGLazy` call as needed
 
-The shader used to render the canvas can be overriden by including script tags
-with `id='shader-frag'` and `type='x-shader/x-fragment'
+The shader used to render the canvas can be overriden via hack options:
 e.g.
-<script id='shader-frag' type='x-shader/x-fragment'>
-	// uv-wave fragment shader
-	precision mediump float;
-	uniform sampler2D tex0;
-	uniform sampler2D tex1;
-	uniform float time;
-	uniform vec2 resolution;
+var hackOptions = {
+	glazyOptions = {
+		fragment: `
+// uv-wave fragment shader
+precision mediump float;
+uniform sampler2D tex0;
+uniform sampler2D tex1;
+uniform float time;
+uniform vec2 resolution;
 
-	void main(){
-		vec2 coord = gl_FragCoord.xy;
-		vec2 uv = coord.xy / resolution.xy;
-		uv.x += sin(uv.y * 10.0 + time / 200.0) / 60.0;
-		uv.y += cos(uv.x * 10.0 + time / 200.0) / 60.0;
-		vec3 col = texture2D(tex0,uv).rgb;
-		gl_FragColor = vec4(col, 1.0);
-	}
-(closing script tag omitted in comment to avoid confusing browser)
+void main(){
+	vec2 coord = gl_FragCoord.xy;
+	vec2 uv = coord.xy / resolution.xy;
+	uv.x += sin(uv.y * 10.0 + time / 200.0) / 60.0;
+	uv.y += cos(uv.x * 10.0 + time / 200.0) / 60.0;
+	vec3 col = texture2D(tex0,uv).rgb;
+	gl_FragColor = vec4(col, 1.0);
+}
+		`,
+	},
+};
 */
 import WebGLazy from "webglazy";
 import {
