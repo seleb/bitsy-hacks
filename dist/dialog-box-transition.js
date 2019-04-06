8aacebdb33a9efa5ddc93e4a3fc328c8371f8b08
Delacannon <delacannon@gmail.com>
2019-04-06 03:53:19 +0200

    Dialog box transition hack (#72)
    * added dialog box transition hack

---
+++ dist/dialog-box-transition.js
@@ -0,0 +1,270 @@
+/**
+🔁
+@file dialog box transition
+@summary adds an easing transition animation to display the dialog box text
+@license MIT
+@version 1.0.0
+@requires 4.8, 4.9
+@author Delacannon
+
+@description
+A hack that adds an easing transition animation to display the dialog box text
+
+HOW TO USE:
+1. Copy-paste this script into a script tag after the bitsy source.
+*/
+this.hacks = this.hacks || {};
+this.hacks.dialog_box_transition = (function (exports,bitsy) {
+'use strict';
+var hackOptions = {
+	easing: 0.025 //  easing speed
+};
+
+bitsy = bitsy && bitsy.hasOwnProperty('default') ? bitsy['default'] : bitsy;
+
+/**
+@file utils
+@summary miscellaneous bitsy utilities
+@author Sean S. LeBlanc
+*/
+
+/*
+Helper used to replace code in a script tag based on a search regex
+To inject code without erasing original string, using capturing groups; e.g.
+	inject(/(some string)/,'injected before $1 injected after')
+*/
+function inject(searchRegex, replaceString) {
+	// find the relevant script tag
+	var scriptTags = document.getElementsByTagName('script');
+	var scriptTag;
+	var code;
+	for (var i = 0; i < scriptTags.length; ++i) {
+		scriptTag = scriptTags[i];
+		var matchesSearch = scriptTag.textContent.search(searchRegex) !== -1;
+		var isCurrentScript = scriptTag === document.currentScript;
+		if (matchesSearch && !isCurrentScript) {
+			code = scriptTag.textContent;
+			break;
+		}
+	}
+
+	// error-handling
+	if (!code) {
+		throw 'Couldn\'t find "' + searchRegex + '" in script tags';
+	}
+
+	// modify the content
+	code = code.replace(searchRegex, replaceString);
+
+	// replace the old script tag with a new one using our modified code
+	var newScriptTag = document.createElement('script');
+	newScriptTag.textContent = code;
+	scriptTag.insertAdjacentElement('afterend', newScriptTag);
+	scriptTag.remove();
+}
+
+/**
+ * Helper for getting an array with unique elements 
+ * @param  {Array} array Original array
+ * @return {Array}       Copy of array, excluding duplicates
+ */
+function unique(array) {
+	return array.filter(function (item, idx) {
+		return array.indexOf(item) === idx;
+	});
+}
+
+/**
+
+@file kitsy-script-toolkit
+@summary makes it easier and cleaner to run code before and after Bitsy functions or to inject new code into Bitsy script tags
+@license WTFPL (do WTF you want)
+@version 3.4.0
+@requires Bitsy Version: 4.5, 4.6
+@author @mildmojo
+
+@description
+HOW TO USE:
+  import {before, after, inject, addDialogTag, addDeferredDialogTag} from "./helpers/kitsy-script-toolkit";
+
+  before(targetFuncName, beforeFn);
+  after(targetFuncName, afterFn);
+  inject(searchRegex, replaceString);
+  addDialogTag(tagName, dialogFn);
+  addDeferredDialogTag(tagName, dialogFn);
+
+  For more info, see the documentation at:
+  https://github.com/seleb/bitsy-hacks/wiki/Coding-with-kitsy
+*/
+
+
+// Ex: inject(/(names.sprite.set\( name, id \);)/, '$1console.dir(names)');
+function inject$1(searchRegex, replaceString) {
+	var kitsy = kitsyInit();
+	kitsy.queuedInjectScripts.push({
+		searchRegex: searchRegex,
+		replaceString: replaceString
+	});
+}
+
+function kitsyInit() {
+	// return already-initialized kitsy
+	if (bitsy.kitsy) {
+		return bitsy.kitsy;
+	}
+
+	// Initialize kitsy
+	bitsy.kitsy = {
+		queuedInjectScripts: [],
+		queuedBeforeScripts: {},
+		queuedAfterScripts: {}
+	};
+
+	var oldStartFunc = bitsy.startExportedGame;
+	bitsy.startExportedGame = function doAllInjections() {
+		// Only do this once.
+		bitsy.startExportedGame = oldStartFunc;
+
+		// Rewrite scripts and hook everything up.
+		doInjects();
+		applyAllHooks();
+
+		// Start the game
+		bitsy.startExportedGame.apply(this, arguments);
+	};
+
+	return bitsy.kitsy;
+}
+
+
+function doInjects() {
+	bitsy.kitsy.queuedInjectScripts.forEach(function (injectScript) {
+		inject(injectScript.searchRegex, injectScript.replaceString);
+	});
+	_reinitEngine();
+}
+
+function applyAllHooks() {
+	var allHooks = unique(Object.keys(bitsy.kitsy.queuedBeforeScripts).concat(Object.keys(bitsy.kitsy.queuedAfterScripts)));
+	allHooks.forEach(applyHook);
+}
+
+function applyHook(functionName) {
+	var functionNameSegments = functionName.split('.');
+	var obj = bitsy;
+	while (functionNameSegments.length > 1) {
+		obj = obj[functionNameSegments.shift()];
+	}
+	var lastSegment = functionNameSegments[0];
+	var superFn = obj[lastSegment];
+	var superFnLength = superFn ? superFn.length : 0;
+	var functions = [];
+	// start with befores
+	functions = functions.concat(bitsy.kitsy.queuedBeforeScripts[functionName] || []);
+	// then original
+	if (superFn) {
+		functions.push(superFn);
+	}
+	// then afters
+	functions = functions.concat(bitsy.kitsy.queuedAfterScripts[functionName] || []);
+
+	// overwrite original with one which will call each in order
+	obj[lastSegment] = function () {
+		var args = [].slice.call(arguments);
+		var i = 0;
+		runBefore.apply(this, arguments);
+
+		// Iterate thru sync & async functions. Run each, finally run original.
+		function runBefore() {
+			// All outta functions? Finish
+			if (i === functions.length) {
+				return;
+			}
+
+			// Update args if provided.
+			if (arguments.length > 0) {
+				args = [].slice.call(arguments);
+			}
+
+			if (functions[i].length > superFnLength) {
+				// Assume funcs that accept more args than the original are
+				// async and accept a callback as an additional argument.
+				functions[i++].apply(this, args.concat(runBefore.bind(this)));
+			} else {
+				// run synchronously
+				var newArgs = functions[i++].apply(this, args);
+				newArgs = newArgs && newArgs.length ? newArgs : args;
+				runBefore.apply(this, newArgs);
+			}
+		}
+	};
+}
+
+function _reinitEngine() {
+	// recreate the script and dialog objects so that they'll be
+	// referencing the code with injections instead of the original
+	bitsy.scriptModule = new bitsy.Script();
+	bitsy.scriptInterpreter = bitsy.scriptModule.CreateInterpreter();
+
+	bitsy.dialogModule = new bitsy.Dialog();
+	bitsy.dialogRenderer = bitsy.dialogModule.CreateRenderer();
+	bitsy.dialogBuffer = bitsy.dialogModule.CreateBuffer();
+}
+
+
+
+
+
+var drawOverride = `
+if(context == null) return;
+		if (isCentered) {
+			context.putImageData(textboxInfo.img, textboxInfo.left*scale, ((height/2)-(textboxInfo.height/2))*scale);
+			this.onExit = ((height/2)-(textboxInfo.height/2))*scale === ((height/2)-(textboxInfo.height/2))*scale
+		}
+		else if (player().y < mapsize/2) {
+			easingDialog(textboxInfo, ${hackOptions.easing}, 
+				!this.onClose ? (height-textboxInfo.bottom-textboxInfo.height)*scale
+				: (height+textboxInfo.bottom+textboxInfo.height)*scale
+				 ) 
+			this.onExit = this.onClose && textboxInfo.y >= (height+textboxInfo.height)*scale
+		}
+		else {
+			easingDialog(textboxInfo, ${
+				hackOptions.easing
+			}, !this.onClose ? textboxInfo.top*scale : 
+				-textboxInfo.top-textboxInfo.height*scale) 
+			 this.onExit = this.onClose && textboxInfo.y <= -textboxInfo.height*scale
+		}
+return;`;
+
+var functionEasing = `
+	function easingDialog(tbox, easing, targetY) {
+		var vy = (targetY - tbox.y) * easing;
+		tbox.y += vy;
+		context.putImageData(tbox.img,tbox.left*scale,tbox.y);
+	}
+	this.onClose = false;
+	this.onExit = false;
+`;
+
+inject$1(
+	/(this\.DrawTextbox\(\))/,
+	`$1\nif(this.onExit && this.onClose){dialogBuffer.EndDialog()}`
+);
+inject$1(/(this\.EndDialog\(\))/, `dialogRenderer.onClose=true`);
+inject$1(/(var DialogRenderer = function\(\) {)/, `$1${functionEasing}`);
+inject$1(/(var textboxInfo = {)/, `$1y:0,`);
+inject$1(
+	/(this\.Reset = function\(\) {)/,
+	`$1 this.onClose=false;
+		this.onExit=false;
+		textboxInfo.y = player().y < mapsize/2 ? (height+textboxInfo.bottom+textboxInfo.height)*scale : -(textboxInfo.height) * scale;`
+);
+
+inject$1(/(this\.DrawTextbox = function\(\) {)/, `$1${drawOverride}`);
+
+exports.hackOptions = hackOptions;
+
+return exports;
+
+}({},window));
---
+++ src/dialog box transition.js
@@ -0,0 +1,69 @@
+/**
+🔁
+@file dialog box transition
+@summary adds an easing transition animation to display the dialog box text
+@license MIT
+@version 1.0.0
+@requires 4.8, 4.9
+@author Delacannon
+
+@description
+A hack that adds an easing transition animation to display the dialog box text
+
+HOW TO USE:
+1. Copy-paste this script into a script tag after the bitsy source.
+*/
+
+import { inject } from "./helpers/kitsy-script-toolkit";
+
+export var hackOptions = {
+	easing: 0.025 //  easing speed
+};
+
+var drawOverride = `
+if(context == null) return;
+		if (isCentered) {
+			context.putImageData(textboxInfo.img, textboxInfo.left*scale, ((height/2)-(textboxInfo.height/2))*scale);
+			this.onExit = ((height/2)-(textboxInfo.height/2))*scale === ((height/2)-(textboxInfo.height/2))*scale
+		}
+		else if (player().y < mapsize/2) {
+			easingDialog(textboxInfo, ${hackOptions.easing}, 
+				!this.onClose ? (height-textboxInfo.bottom-textboxInfo.height)*scale
+				: (height+textboxInfo.bottom+textboxInfo.height)*scale
+				 ) 
+			this.onExit = this.onClose && textboxInfo.y >= (height+textboxInfo.height)*scale
+		}
+		else {
+			easingDialog(textboxInfo, ${
+				hackOptions.easing
+			}, !this.onClose ? textboxInfo.top*scale : 
+				-textboxInfo.top-textboxInfo.height*scale) 
+			 this.onExit = this.onClose && textboxInfo.y <= -textboxInfo.height*scale
+		}
+return;`;
+
+var functionEasing = `
+	function easingDialog(tbox, easing, targetY) {
+		var vy = (targetY - tbox.y) * easing;
+		tbox.y += vy;
+		context.putImageData(tbox.img,tbox.left*scale,tbox.y);
+	}
+	this.onClose = false;
+	this.onExit = false;
+`;
+
+inject(
+	/(this\.DrawTextbox\(\))/,
+	`$1\nif(this.onExit && this.onClose){dialogBuffer.EndDialog()}`
+);
+inject(/(this\.EndDialog\(\))/, `dialogRenderer.onClose=true`);
+inject(/(var DialogRenderer = function\(\) {)/, `$1${functionEasing}`);
+inject(/(var textboxInfo = {)/, `$1y:0,`);
+inject(
+	/(this\.Reset = function\(\) {)/,
+	`$1 this.onClose=false;
+		this.onExit=false;
+		textboxInfo.y = player().y < mapsize/2 ? (height+textboxInfo.bottom+textboxInfo.height)*scale : -(textboxInfo.height) * scale;`
+);
+
+inject(/(this\.DrawTextbox = function\(\) {)/, `$1${drawOverride}`);
