f64884f1656006f233bc6bc74370fd14be0d258f
Sean LeBlanc <sean.s.leblanc@gmail.com>
2019-04-14 14:34:35 -0400

    Exit from dialog for v6.0 (#76)
    * use bitsy 6.0 exit function instead of hack implementation
    
    * remove sprite coords + match bitsy more closely
    
    * add support for `transition_effect`
    
    * update docs
    
    * run build

--- src/exit-from-dialog.js
+++ src/exit-from-dialog.js
@@ -3,13 +3,13 @@
 @file exit-from-dialog
 @summary exit to another room from dialog, including conditionals
 @license WTFPL (do WTF you want)
-@version 5.2.2
-@requires Bitsy Version: 4.5, 4.6
+@version 6.0.0
+@requires Bitsy Version: 6.0
 @author @mildmojo
 
 @description
-Lets you exit to another room from dialog (including inside conditionals). Use
-it to make an invisible sprite that acts as a conditional exit, use it to warp
+Lets you exit to another room from dialog (including inside conditionals).
+Use it to make an invisible sprite that acts as a conditional exit, use it to warp
 somewhere after a conversation, use it to put a guard at your gate who only
 lets you in once you're disguised, use it to require payment before the
 ferryman will take you across the river.
@@ -18,32 +18,28 @@ Using the (exit) function in any part of a series of dialog will make the
 game exit to the new room after the dialog is finished. Using (exitNow) will
 immediately warp to the new room, but the current dialog will continue.
 
-WARNING: In exit coordinates, the TOP LEFT tile is (0,0). In sprite coordinates,
-         the BOTTOM LEFT tile is (0,0). If you'd like to use sprite coordinates,
-         add the word "sprite" as the fourth parameter to the exit function.
+Usage:
+	(exit "<room name>,<x>,<y>,<optional transition_effect>")
+	(exitNow "<room name>,<x>,<y>,<optional transition_effect>")
 
-Usage: (exit "<room name>,<x>,<y>")
-       (exit "<room name>,<x>,<y>,sprite")
-       (exitNow "<room name>,<x>,<y>")
-       (exitNow "<room name>,<x>,<y>,sprite")
-
-Example: (exit "FinalRoom,8,4")
-         (exitNow "FinalRoom,8,11,sprite")
+Example:
+	(exit "FinalRoom,8,4")
+	(exit "FinalRoom,8,4,tunnel")
 
 HOW TO USE:
-  1. Copy-paste this script into a new script tag after the Bitsy source code.
-     It should appear *before* any other mods that handle loading your game
-     data so it executes *after* them (last-in first-out).
+1. Copy-paste this script into a new script tag after the Bitsy source code.
+   It should appear *before* any other mods that handle loading your game
+   data so it executes *after* them (last-in first-out).
 
-NOTE: This uses parentheses "()" instead of curly braces "{}" around function
-      calls because the Bitsy editor's fancy dialog window strips unrecognized
-      curly-brace functions from dialog text. To keep from losing data, write
-      these function calls with parentheses like the examples above.
+NOTE:
+This uses parentheses "()" instead of curly braces "{}" around function
+calls because the Bitsy editor's fancy dialog window strips unrecognized
+curly-brace functions from dialog text. To keep from losing data, write
+these function calls with parentheses like the examples above.
 
-      For full editor integration, you'd *probably* also need to paste this
-      code at the end of the editor's `bitsy.js` file. Untested.
+For full editor integration, you'd *probably* also need to paste this
+code at the end of the editor's `bitsy.js` file. Untested.
 */
-'use strict';
 import bitsy from "bitsy";
 import {
 	getRoom
@@ -54,12 +50,11 @@ import {
 
 // Implement the dialog functions
 addDualDialogTag('exit', function (environment, parameters) {
-	var exitParams = _getExitParams(parameters);
-	if (!exitParams) {
+	var exit = _getExitParams(parameters);
+	if (!exit) {
 		return;
 	}
-
-	doPlayerExit(exitParams);
+	bitsy.movePlayerThroughExit(exit);
 });
 
 function _getExitParams(parameters) {
@@ -67,32 +62,26 @@ function _getExitParams(parameters) {
 	var roomName = params[0];
 	var x = params[1];
 	var y = params[2];
-	var coordsType = (params[3] || 'exit').toLowerCase();
-	var useSpriteCoords = coordsType === 'sprite';
-	var roomId = getRoom(roomName).id;
+	var transition_effect = params[3];
+	var room = getRoom(roomName).id;
 
 	if (!roomName || x === undefined || y === undefined) {
 		console.warn('{exit/exitNow} was missing parameters! Usage: {exit/exitNow "roomname,x,y"}');
 		return null;
 	}
 
-	if (roomId === undefined) {
+	if (room === undefined) {
 		console.warn("Bad {exit/exitNow} parameter: Room '" + roomName + "' not found!");
 		return null;
 	}
 
 	return {
-		room: roomId,
-		x: Number(x),
-		y: useSpriteCoords ? 15 - Number(y) : Number(y)
+		dest: {
+			room,
+			x: Number(x),
+			y: Number(y),
+		},
+		transition_effect,
 	};
-}
-
-// dest === {room: Room, x: Int, y: Int}
-function doPlayerExit(dest) {
-	bitsy.player().room = dest.room;
-	bitsy.player().x = dest.x;
-	bitsy.player().y = dest.y;
-	bitsy.curRoom = dest.room;
 }
 // End of (exit) dialog function mod
