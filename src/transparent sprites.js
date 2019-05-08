5ba34b9c2e64f952e75160780fb1a61284ac1581
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-05-07 23:47:39 -0400

    rewrite transparent sprites to take advantage of bitsy drawing canvases directly

--- src/transparent sprites.js
+++ src/transparent sprites.js
@@ -18,7 +18,6 @@ HOW TO USE:
 import bitsy from "bitsy";
 import {
 	before,
-	after,
 } from "./helpers/kitsy-script-toolkit";
 
 export var hackOptions = {
@@ -30,78 +29,38 @@ export var hackOptions = {
 	},
 };
 
-// override renderer.GetImage to create + cache
-// and always give it the player to prevent it from drawing the original assets
-var imgToDraw;
-var imgCache = {};
+var madeTransparent = {};
+var makeTransparent = false;
 before('renderer.GetImage', function (drawing, paletteId, frameOverride) {
-	var returnVal = [window.player(), 0]; // consistent bitsy getter to reduce rendering costs
-
 	// check cache first
-	var cache = imgCache[drawing.drw] = imgCache[drawing.drw] || {};
+	var cache = madeTransparent[drawing.drw] = madeTransparent[drawing.drw] || {};
 	var p = cache[paletteId] = cache[paletteId] || {};
 	var frameIndex = frameOverride || drawing.animation.frameIndex;
-	imgToDraw = p[frameIndex];
-	if (imgToDraw) {
-		return returnVal;
+	if (p[frameIndex]) {
+		// already made this transparent
+		return;
 	}
 
-	// get the vars we need
-	var alpha = hackOptions.isTransparent(drawing) ? 0 : 255;
-	var bg = bitsy.getPal(paletteId)[0];
-	var col = bitsy.getPal(paletteId)[drawing.col];
-	var imageSource = bitsy.renderer.GetImageSource(drawing.drw)[frameIndex];
-	var x, y, i, j, pixel, idx;
-	var size = bitsy.tilesize * bitsy.scale;
+	// flag the next draw as needing to be made transparent
+	p[frameIndex] = makeTransparent = hackOptions.isTransparent(drawing);
+});
 
-	// give ourselves a little canvas + context to work with
-	var spriteCanvas = document.createElement("canvas");
-	spriteCanvas.width = size;
-	spriteCanvas.height = size;
-	var spriteContext = spriteCanvas.getContext("2d");
-	var img = spriteContext.createImageData(size, size);
-
-	// create image data
-	for (y = 0; y < bitsy.tilesize; ++y) {
-		for (x = 0; x < bitsy.tilesize; ++x) {
-			pixel = !!imageSource[y][x];
-			for (i = 0; i < bitsy.scale; ++i) {
-				for (j = 0; j < bitsy.scale; ++j) {
-					idx = ((x * bitsy.scale + j) + (y * bitsy.scale + i) * size) * 4;
-					img.data[idx] = pixel ? col[0] : bg[0];
-					img.data[idx + 1] = pixel ? col[1] : bg[1];
-					img.data[idx + 2] = pixel ? col[2] : bg[2];
-					img.data[idx + 3] = pixel ? 255 : alpha;
-				}
+before('drawTile', function (canvas) {
+	if (makeTransparent) {
+		// redraw with all bg pixels transparent
+		var ctx = canvas.getContext('2d');
+		var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		var bg = bitsy.getPal(bitsy.curPal())[0];
+		for (let i = 0; i < data.data.length; i += 4) {
+			var r = data.data[i];
+			var g = data.data[i + 1];
+			var b = data.data[i + 2];
+			if (r === bg[0] && g === bg[1] && b === bg[2]) {
+				data.data[i + 3] = 0;
 			}
 		}
+		ctx.putImageData(data, 0, 0);
+		// clear the flag
+		makeTransparent = false;
 	}
-
-	// put data to our canvas
-	spriteContext.clearRect(0, 0, size, size);
-	spriteContext.putImageData(img, 0, 0);
-	document.body.appendChild(spriteCanvas);
-
-	// save it in our cache
-	imgToDraw = p[frameIndex] = spriteCanvas;
-	return imgToDraw;
-});
-// return our custom image instead of the original image data
-after('renderer.GetImage', function () {
-	return imgToDraw;
-});
-
-// override drawTile to draw from our custom image cache
-// and give it a mock context to prevent the original drawing
-var mockContext = {
-	drawImage: function () {},
-};
-before('drawTile', function (img, x, y, context) {
-	if (!context) { //optional pass in context; otherwise, use default
-		context = bitsy.ctx;
-	}
-	// draw our custom image
-	context.drawImage(img, x * bitsy.tilesize * bitsy.scale, y * bitsy.tilesize * bitsy.scale);
-	// prevent bitsy from drawing
-	return [img, x, y, mockContext];
 });
