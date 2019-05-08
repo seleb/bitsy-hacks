39e718bd86df9776b523ddf968f68af2c9dfe634
Sean LeBlanc <sean.s.leblanc@gmail.com>
2019-05-08 00:11:13 -0400

    update transparent sprites for 6.1 (#81)
    * Update transparent sprites.js
    
    * rewrite transparent sprites to take advantage of bitsy drawing canvases directly
    
    * bump transparent sprite version
    
    requires 6.1's renderer; major version bump since it's not backwards compatible
    
    * update the test file
    
    * Revert "update close on ending snapshots"
    
    This reverts commit b2d741f757ded15ff5b27555db30b1b55e66fbd3.
    
    * update close on ending snapshots
    
    committed the diff last time :neutral_face:
    
    * build
    
    * 2.0.0

--- src/test/bitsy.js
+++ src/test/bitsy.js
@@ -33,7 +33,7 @@ async function getHackDist() {
 }
 
 async function getHackTemplate() {
-	return readFile(resolve(__dirname, 'bitsy 6.0.html'), {
+	return readFile(resolve(__dirname, 'bitsy 6.1.html'), {
 		encoding: 'utf8'
 	});
 }
