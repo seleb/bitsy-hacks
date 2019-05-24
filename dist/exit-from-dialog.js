405d0dbf95d036d394fb8838f30d47f0e1d9de88
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-05-24 10:25:31 -0400

    3.1.0

--- dist/exit-from-dialog.js
+++ dist/exit-from-dialog.js
@@ -3,7 +3,7 @@
 @file exit-from-dialog
 @summary exit to another room from dialog, including conditionals
 @license WTFPL (do WTF you want)
-@version 6.0.0
+@version 6.1.0
 @requires Bitsy Version: 6.0
 @author @mildmojo
 
