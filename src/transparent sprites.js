0ebb9a8ff11fb71a3d46a7cc31f23debf4eb43d3
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-05-27 20:14:16 -0400

    fix flag in name example (`name` can be `null`)

--- src/transparent sprites.js
+++ src/transparent sprites.js
@@ -24,7 +24,7 @@ export var hackOptions = {
 	isTransparent: function (drawing) {
 		//return drawing.name == 'tea'; // specific transparent drawing
 		//return ['tea', 'flower', 'hat'].indexOf(drawing.name) !== -1; // specific transparent drawing list
-		//return drawing.name.indexOf('TRANSPARENT') !== -1; // transparent drawing flag in name
+		//return drawing.name && drawing.name.indexOf('TRANSPARENT') !== -1; // transparent drawing flag in name
 		return true; // all drawings are transparent
 	},
 };
