b1fbec69371157308d9e26bceab7617a0d94101d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-05-28 20:34:39 -0400

    build

--- dist/long-dialog.js
+++ dist/long-dialog.js
@@ -1,38 +1,36 @@
 /**
-📃
-@file paragraph-break
-@summary Adds paragraph breaks to the dialogue parser
-@license WTFPL (do WTF you want)
-@version 1.1.2
-@requires Bitsy Version: 5.0, 5.1
-@author Sean S. LeBlanc, David Mowatt
+📜
+@file long dialog
+@summary put more words onscreen
+@license MIT
+@version 1.0.0
+@requires Bitsy Version: 6.1
+@author Sean S. LeBlanc
 
 @description
-Adds a (p) tag to the dialogue parser that forces the following text to 
-start on a fresh dialogue screen, eliminating the need to spend hours testing
-line lengths or adding multiple line breaks that then have to be reviewed
-when you make edits or change the font size.
+Makes the dialog box variable in height, allowing it to expand as needed.
 
-Usage: (p)
-       
-Example: I am a cat(p)and my dialogue contains multitudes
+Minimum and maximum size are configurable.
+Cheat sheet:
+	2: bitsy default
+	8: reaches just below the halfway mark
+	16: roughly the max of the original bitsy margins
+	19: max before cutting off text
+
+Note: if you want to use this hack but still maintain the appearance of original bitsy dialog behaviour in some areas,
+you can combine it with the paragraph break hack to manually add breaks every two lines.
 
 HOW TO USE:
-  1. Copy-paste this script into a new script tag after the Bitsy source code.
-     It should appear *before* any other mods that handle loading your game
-     data so it executes *after* them (last-in first-out).
-
-NOTE: This uses parentheses "()" instead of curly braces "{}" around function
-      calls because the Bitsy editor's fancy dialog window strips unrecognized
-      curly-brace functions from dialog text. To keep from losing data, write
-      these function calls with parentheses like the examples above.
-
-      For full editor integration, you'd *probably* also need to paste this
-      code at the end of the editor's `bitsy.js` file. Untested.
+	1. Copy-paste this script into a new script tag after the Bitsy source code.
+	2. edit hackOptions below as needed
 */
 this.hacks = this.hacks || {};
-(function (bitsy) {
+this.hacks.long_dialog = (function (exports,bitsy) {
 'use strict';
+var hackOptions = {
+	minRows: 2,
+	maxRows: 4,
+};
 
 bitsy = bitsy && bitsy.hasOwnProperty('default') ? bitsy['default'] : bitsy;
 
@@ -119,15 +117,6 @@ function inject$1(searchRegex, replaceString) {
 		searchRegex: searchRegex,
 		replaceString: replaceString
 	});
-}
-
-// Ex: before('load_game', function run() { alert('Loading!'); });
-//     before('show_text', function run(text) { return text.toUpperCase(); });
-//     before('show_text', function run(text, done) { done(text.toUpperCase()); });
-function before(targetFuncName, beforeFn) {
-	var kitsy = kitsyInit();
-	kitsy.queuedBeforeScripts[targetFuncName] = kitsy.queuedBeforeScripts[targetFuncName] || [];
-	kitsy.queuedBeforeScripts[targetFuncName].push(beforeFn);
 }
 
 function kitsyInit() {
@@ -237,69 +226,23 @@ function _reinitEngine() {
 	bitsy.dialogBuffer = bitsy.dialogModule.CreateBuffer();
 }
 
-// Rewrite custom functions' parentheses to curly braces for Bitsy's
-// interpreter. Unescape escaped parentheticals, too.
-function convertDialogTags(input, tag) {
-	return input
-		.replace(new RegExp('\\\\?\\((' + tag + '(\\s+(".+?"|.+?))?)\\\\?\\)', 'g'), function(match, group){
-			if(match.substr(0,1) === '\\') {
-				return '('+ group + ')'; // Rewrite \(tag "..."|...\) to (tag "..."|...)
-			}
-			return '{'+ group + '}'; // Rewrite (tag "..."|...) to {tag "..."|...}
-		});
-}
-
-
-function addDialogFunction(tag, fn) {
-	var kitsy = kitsyInit();
-	kitsy.dialogFunctions = kitsy.dialogFunctions || {};
-	if (kitsy.dialogFunctions[tag]) {
-		throw new Error('The dialog function "' + tag + '" already exists.');
-	}
-
-	// Hook into game load and rewrite custom functions in game data to Bitsy format.
-	before('parseWorld', function (game_data) {
-		return [convertDialogTags(game_data, tag)];
-	});
-
-	kitsy.dialogFunctions[tag] = fn;
-}
-
-/**
- * Adds a custom dialog tag which executes the provided function.
- * For ease-of-use with the bitsy editor, tags can be written as
- * (tagname "parameters") in addition to the standard {tagname "parameters"}
- * 
- * Function is executed immediately when the tag is reached.
- *
- * @param {string}   tag Name of tag
- * @param {Function} fn  Function to execute, with signature `function(environment, parameters, onReturn){}`
- *                       environment: provides access to SetVariable/GetVariable (among other things, see Environment in the bitsy source for more info)
- *                       parameters: array containing parameters as string in first element (i.e. `parameters[0]`)
- *                       onReturn: function to call with return value (just call `onReturn(null);` at the end of your function if your tag doesn't interact with the logic system)
- */
-function addDialogTag(tag, fn) {
-	addDialogFunction(tag, fn);
-	inject$1(
-		/(var functionMap = new Map\(\);)/,
-		'$1functionMap.set("' + tag + '", kitsy.dialogFunctions.' + tag + ');'
-	);
-}
-
-/**
- * Helper for printing a paragraph break inside of a dialog function.
- * Adds the function `AddParagraphBreak` to `DialogBuffer`
- */
-
-inject$1(/(this\.AddLinebreak = )/, 'this.AddParagraphBreak = function() { buffer.push( [[]] ); isActive = true; };\n$1');
 
 
 
-//Adds the actual dialogue tag. No deferred version is required.
-addDialogTag('p', function(environment, parameters, onReturn){
-    environment.GetDialogBuffer().AddParagraphBreak();
-    onReturn(null);
-});
-// End of (p) paragraph break mod
 
-}(window));
+// override textbox height
+inject$1(/textboxInfo\.height = .+;/,
+`Object.defineProperty(textboxInfo, 'height', {
+	get() { return textboxInfo.padding_vert + (textboxInfo.padding_vert + relativeFontHeight()) * Math.max(${hackOptions.minRows}, dialogBuffer.CurPage().indexOf(dialogBuffer.CurRow())+Math.sign(dialogBuffer.CurCharCount())) + textboxInfo.arrow_height; }
+})`);
+// prevent textbox from caching
+inject$1(/(if\(textboxInfo\.img == null\))/, '// $1');
+// rewrite hard-coded row limit
+inject$1(/(else if \(curRowIndex )== 0/g, '$1< ' + hackOptions.maxRows + ' - 1');
+inject$1(/(if\( lastPage\.length) <= 1( \) {)/, '$1 < ' + hackOptions.maxRows + ' $2');
+
+exports.hackOptions = hackOptions;
+
+return exports;
+
+}({},window));
