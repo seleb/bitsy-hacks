f0d559d2b6102b51ff2c9a59539b9394d39f82d7
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-08-24 12:04:10 -0400

    rewrite smooth moves to support animation of arbitrary sprites

--- src/smooth moves.js
+++ src/smooth moves.js
@@ -3,7 +3,7 @@
 @file smooth moves
 @summary ease the player's movement
 @license MIT
-@version 1.0.3
+@version 2.0.0
 @requires Bitsy Version: 6.3
 @author Sean S. LeBlanc
 
@@ -24,6 +24,8 @@ import bitsy from 'bitsy';
 export var hackOptions = {
 	// duration of ease in ms
 	duration: 100,
+	// max distance to allow tweens
+	delta: 1.5,
 	// easing function
 	ease: function(t) {
 		t = 1 - Math.pow(1 - t, 2);
@@ -32,61 +34,64 @@ export var hackOptions = {
 };
 
 // smooth move
-var fromX;
-var fromY;
-var toX;
-var toY;
-var dir;
-var lastMove;
+var tweens = {};
+var sprites = {};
+function addTween(spr, fromX, fromY, toX, toY) {
+	if (Math.abs(toX - fromX) + Math.abs(toY - fromY) > hackOptions.delta) {
+		delete tweens[spr];
+	} else {
+		var t = tweens[spr] = tweens[spr] || {};
+		t.fromX = fromX;
+		t.fromY = fromY;
+		t.toX = toX;
+		t.toY = toY;
+		t.start = bitsy.prevTime;
+	}
+}
 before('onready', function() {
-	fromX = toX = bitsy.player().x;
-	fromY = toY = bitsy.player().y;
-	lastMove = bitsy.prevTime;
+	tweens = {};
+	sprites = {};
 });
-before('movePlayer', function () {
-	bitsy.player().x = toX;
-	bitsy.player().y = toY;
-	fromX = bitsy.player().x;
-	fromY = bitsy.player().y;
+
+// listen for changes in sprite positions to add tweens
+before('update', function() {
+	Object.values(bitsy.sprite).forEach(spr => {
+		if (spr.room === bitsy.curRoom) {
+			var s = sprites[spr.id] = sprites[spr.id] || {};
+			s.x = spr.x;
+			s.y = spr.y;
+		} else {
+			delete sprites[spr.id];
+		}
+	});
 });
-after('movePlayer', function () {
-	dir = bitsy.curPlayerDirection;
-	toX = bitsy.player().x;
-	toY = bitsy.player().y;
-	lastMove = bitsy.prevTime;
-});
-var px;
-var py;
-after('update', function() {
-	px = bitsy.player().x;
-	py = bitsy.player().y;
-});
+function addTweens() {
+	Object.entries(sprites).forEach(function (entry) {
+		var spr = bitsy.sprite[entry[0]];
+		var pos = entry[1];
+		if (pos.x !== spr.x || pos.y !== spr.y) {
+			addTween(spr.id, pos.x, pos.y, spr.x, spr.y);
+		}
+	});
+}
+after('updateInput', addTweens);
+after('update', addTweens);
+// before drawing, update sprite positions to tweened values
 before('drawRoom', function () {
-	if (bitsy.player().x !== px || bitsy.player().y !== py) {
-		if (bitsy.player().x % 1 === 0 && bitsy.player().y % 1 === 0) {
-			toX = bitsy.player().x;
-			toY = bitsy.player().y;
-		}
-	}
-	var t = Math.min(1, (bitsy.prevTime - lastMove) / hackOptions.duration);
-	var dx = 0;
-	var dy = 0;
-	if (Math.abs(toX - fromX) > 0) {
-		if(dir === bitsy.Direction.Left) {
-			dx -= 1;
-		}
-		if(dir === bitsy.Direction.Right) {
-			dx += 1;
-		}
-	}
-	if (Math.abs(toY - fromY) > 0) {
-		if(dir === bitsy.Direction.Up) {
-			dy -= 1;
-		}
-		if(dir === bitsy.Direction.Down) {
-			dy += 1;
-		}
-	}
-	bitsy.player().x = toX - Math.sign(dx) * (1.0 - hackOptions.ease(t));
-	bitsy.player().y = toY - Math.sign(dy) * (1.0 - hackOptions.ease(t));
+	Object.entries(tweens).forEach(function (entry) {
+		var tween = entry[1];
+		var t = hackOptions.ease(Math.min(1, (bitsy.prevTime - tween.start) / hackOptions.duration));
+		var sprite = bitsy.sprite[entry[0]];
+		sprite.x = tween.fromX + (tween.toX - tween.fromX) * t;
+		sprite.y = tween.fromY + (tween.toY - tween.fromY) * t;
+	});
+});
+// after drawing, update sprite positions back to normal
+after('drawRoom', function () {
+	Object.entries(tweens).forEach(function (entry) {
+		var tween = entry[1];
+		var sprite = bitsy.sprite[entry[0]];
+		sprite.x = tween.toX;
+		sprite.y = tween.toY;
+	});
 });
