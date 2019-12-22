6f9ac88a1dcebcd1e62acfc3b47fa4709ff3b9d2
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-12-21 19:13:04 -0500

    update versions + build
    Co-Authored-By: ducklingsmith <ducklingsmith@users.noreply.github.com>

--- src/edit image from dialog.js
+++ src/edit image from dialog.js
@@ -3,7 +3,7 @@
 @file edit image from dialog
 @summary edit sprites, items, and tiles from dialog
 @license MIT
-@version 1.2.7
+@version 1.2.8
 @requires 5.3
 @author Sean S. LeBlanc
 
--- src/online.js
+++ src/online.js
@@ -3,7 +3,7 @@
 @file online
 @summary multiplayer bitsy
 @license MIT
-@version 2.1.8
+@version 2.1.9
 @requires 5.5
 @author Sean S. LeBlanc
 @description
