97963a2632a6830ef228bde06717e910c03b8973
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-04-25 00:50:58 -0400

    permanent items test

---
+++ src/permanent items.test.js
@@ -0,0 +1,91 @@
+import {
+	start,
+	press,
+	end,
+	snapshot,
+} from './test/bitsy';
+
+test('p', async () => {
+	await start({
+		gamedata: `
+
+# BITSY VERSION 7.0
+
+! ROOM_FORMAT 1
+
+PAL 0
+NAME blueprint
+0,82,204
+128,159,255
+255,255,255
+
+ROOM 0
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+0,a,a,a,a,a,a,a,a,a,a,a,a,a,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,0,0,0,0,0,0,0,0,0,0,0,0,a,0
+0,a,a,a,a,a,a,a,a,a,a,a,a,a,a,0
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+NAME example room
+ITM 0 5,4
+PAL 0
+
+TIL a
+11111111
+10000001
+10000001
+10011001
+10011001
+10000001
+10000001
+11111111
+NAME block
+
+SPR A
+00011000
+00011000
+00011000
+00111100
+01111110
+10111101
+00100100
+00100100
+POS 0 4,4
+
+ITM 0
+00000000
+00000000
+00000000
+00111100
+01100100
+00100100
+00011000
+00000000
+NAME tea
+DLG 1
+
+DLG 1
+You found a nice warm cup of tea
+NAME tea dialog
+
+VAR a
+42`,
+		hacks: ['permanent items'],
+	});
+	await press('ArrowRight'); // walk onto item
+	await press('Enter'); // complete dialog
+	await press('Enter'); // end dialog
+	await press('ArrowRight'); // walk off item
+	await snapshot();
+	await end();
+});
