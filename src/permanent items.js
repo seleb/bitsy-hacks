a241b097b30956f0a1e93b483e9bb92e8a7a0081
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-04-25 01:10:40 -0400

    bump versions

--- src/permanent items.js
+++ src/permanent items.js
@@ -3,7 +3,7 @@
 @file permanent items
 @summary prevent some items from being picked up
 @license MIT
-@version 2.1.6
+@version 3.0.0
 @author Sean S. LeBlanc
 
 @description
--- src/solid items.js
+++ src/solid items.js
@@ -3,7 +3,7 @@
 @file solid items
 @summary treat some items like sprites that can be placed multiple times
 @license MIT
-@version 2.1.6
+@version 3.0.0
 @author Sean S. LeBlanc
 
 @description
