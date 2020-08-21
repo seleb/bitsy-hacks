d1c6cf1875319d72bfd6453a9fdc74cf21a9281a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-08-21 02:44:38 -0400

    test sprite effects

---
+++ src/sprite effects.test.js
@@ -0,0 +1,40 @@
+import { end, press, snapshot, start, walkToCat } from './test/bitsy';
+
+test('sprite effects', async () => {
+	await start({
+		catDialog: '\\(spriteEffect "SPR,A,invert"\\){spriteEffect "SPR,A,invert"}',
+		hacks: [
+			'sprite effects',
+		],
+	});
+	await walkToCat();
+	await press('ArrowRight'); // talk to cat
+	await press('Enter'); // complete dialog page
+	await press('Enter'); // end dialog
+	await snapshot();
+	await press('ArrowRight'); // talk to cat
+	await press('Enter'); // complete dialog page
+	await press('Enter'); // end dialog
+	await snapshot();
+	await end();
+});
+
+test('compatible with transparency', async () => {
+	await start({
+		catDialog: '\\(spriteEffect "SPR,A,invert"\\){spriteEffect "SPR,A,invert"}',
+		hacks: [
+			'transparent sprites',
+			'sprite effects',
+		],
+	});
+	await walkToCat();
+	await press('ArrowRight'); // talk to cat
+	await press('Enter'); // complete dialog page
+	await press('Enter'); // end dialog
+	await snapshot();
+	await press('ArrowRight'); // talk to cat
+	await press('Enter'); // complete dialog page
+	await press('Enter'); // end dialog
+	await snapshot();
+	await end();
+});
