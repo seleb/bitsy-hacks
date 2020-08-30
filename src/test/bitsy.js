24cc7a3ef5a5b9b81dc37f6c70d3aa3e2f8dc74f
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-08-30 12:32:46 -0400

    test: 7.1 -> 7.2

--- src/test/bitsy.js
+++ src/test/bitsy.js
@@ -34,7 +34,7 @@ async function getHackDist() {
 }
 
 async function getHackTemplate() {
-	return readFile(resolve(__dirname, 'bitsy 7.1.html'), {
+	return readFile(resolve(__dirname, 'bitsy 7.2.html'), {
 		encoding: 'utf8',
 	});
 }
