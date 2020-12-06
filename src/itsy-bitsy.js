f765586e7df8e281631620c25a425e535ceabef6
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-12-05 20:33:24 -0500

    add custom text row length back in

--- src/itsy-bitsy.js
+++ src/itsy-bitsy.js
@@ -12,13 +12,25 @@ Modifies bitsy to run at 64x64 pixels instead of 256x256.
 
 Note that this means you have significantly less space for text
 (text in regular bitsy is twice as large as the rest of the game)
+To help deal with this, a hack option is provided which lets you
+customize how many rows of text the dialog boxes will show.
 
 HOW TO USE:
 	1. Copy-paste this script into a new script tag after the Bitsy source code.
+	2. edit hackOptions below as needed
+
+NOTE:
+The number of rows is the only provided hack option,
+but most of the numbers being replaced can be easily
+customized if you want slightly different sizes/positions.
 */
 import {
 	inject,
 } from './helpers/kitsy-script-toolkit';
+
+export var hackOptions = {
+	rows: 2, // number of rows per text box (bitsy default is 2)
+};
 
 // rewrite main canvas width/height
 inject(/(width =) 128/, '$1 64');
@@ -44,6 +56,8 @@ inject(/(var textboxInfo = {)[^]*?(};)/, '$1' + [
 
 inject(/\(4 \* textToRoomScaleRatio\)/g, '1');
 inject(/(pixelsPerRow =) .*;/, '$1 62;'); // rewrite hard-coded textbox wrap width
+inject(/(relativeFontHeight\(\) \*) 2/, '$1 ' + hackOptions.rows); // rewrite textbox height
+inject(/(var maxRowCount = )2/, '$1 ' + hackOptions.rows); // rewrite hard-coded row limit
 
 // inject pixelated rendering style
 var style = document.createElement('style');
