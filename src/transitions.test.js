59998bceed5c18c3f332b05f19471131af09a858
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-12-06 02:16:00 -0500

    test: add test for transitions hack

---
+++ src/transitions.test.js
@@ -0,0 +1,51 @@
+import {
+	delay,
+	end,
+	press,
+	snapshot,
+	start,
+} from './test/bitsy';
+
+test('transitions', async () => {
+	await start({
+		hacks: [
+			[
+				'transitions',
+				{
+					duration: 2000,
+					includeTitle: false,
+					checkTransition: function () {
+						var x = window.player().x;
+						if (this.x !== x) {
+							this.x = x;
+							return true;
+						}
+						return false;
+					},
+					transition: `
+if (t < 0.01 || t > 0.99) {
+	result = end;
+} else {
+	result = mix(vec3(1.0,0.0,0.0), vec3(0.0,1.0,0.0), step(0.5, t));
+}
+`,
+					glazyOptions: {
+						scaleMode: 'FIT',
+						allowDownscaling: true,
+					},
+				},
+			],
+		],
+	});
+	await press('Enter'); // complete title page
+	await press('Enter'); // end title page
+	await delay(2000);
+	await snapshot();
+	await press('ArrowRight');
+	await snapshot();
+	await delay(1000);
+	await snapshot();
+	await delay(1000);
+	await snapshot();
+	await end();
+});
