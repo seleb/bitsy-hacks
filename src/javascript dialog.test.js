e5ad7705c63488bd0ab958c8fc820606f1523c44
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-12-05 21:38:35 -0500

    test: javascript dialog

---
+++ src/javascript dialog.test.js
@@ -0,0 +1,33 @@
+import {
+	end,
+	press,
+	snapshot,
+	start,
+	walkToCat,
+} from './test/bitsy';
+
+test('now', async () => {
+	await start({
+		hacks: ['javascript dialog'],
+		catDialog: '{jsNow "canvas.width *= 2"}I\'m a cat',
+	});
+	await walkToCat();
+	await press('ArrowRight'); // talk to cat
+	await press('Enter'); // complete dialog page
+	await snapshot();
+	await end();
+});
+
+test('deferred', async () => {
+	await start({
+		hacks: ['javascript dialog'],
+		catDialog: '{js "canvas.width *= 2"}I\'m a cat',
+	});
+	await walkToCat();
+	await press('ArrowRight'); // talk to cat
+	await press('Enter'); // complete dialog page
+	await snapshot();
+	await press('Enter'); // close dialog page
+	await snapshot();
+	await end();
+});
