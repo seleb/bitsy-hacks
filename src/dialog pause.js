89f86dc3c4298f6ef0eac8bd8c7593cb728301aa
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-12-11 00:01:19 -0500

    update dialog pause

--- src/dialog pause.js
+++ src/dialog pause.js
@@ -4,12 +4,13 @@
 @summary add pauses in between printing text
 @license MIT
 @version auto
+@requires 8.0
 @author Sean S. LeBlanc
 
 @description
 Adds a command that allows you to add pauses in between printing text.
 
-Example: (pause "1000")
+Example: (pause 1000)
 
 Note: pause times are in milliseconds
 
@@ -17,9 +18,11 @@ HOW TO USE:
 Copy-paste into a script tag after the bitsy source
 */
 import bitsy from 'bitsy';
-import { addDialogTag } from './helpers/kitsy-script-toolkit';
+import { addDialogTag, inject } from './helpers/kitsy-script-toolkit';
 
 addDialogTag('pause', function (parameters, onReturn) {
 	bitsy.dialogBuffer.Update(-parseFloat(parameters[0]));
 	onReturn(false);
 });
+
+inject(/nextCharTimer = nextCharMaxTime/, 'nextCharTimer = nextCharTimer < 0 ? nextCharTimer : nextCharMaxTime');
