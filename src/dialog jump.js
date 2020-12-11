0bb80f93dc27699041da16e4a6f298990eabeb80
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-12-10 23:09:04 -0500

    update dialog jump
    note: slightly different functionality

--- src/dialog jump.js
+++ src/dialog jump.js
@@ -4,7 +4,7 @@
 @summary jump from one dialog entry to another
 @license MIT
 @version auto
-@requires 7.0
+@requires 8.0
 @author Sean S. LeBlanc
 
 @description
@@ -12,20 +12,14 @@ This can be used to simplify complex dialog
 by moving portions to self-contained dialog entries,
 and then jumping to the appropriate id when necessary.
 
-You can also provide raw dialog text instead of an id;
-Functionally this isn't much different from writing raw dialog text,
-but it has some uses for advanced cases (e.g. when combined with dialog choices)
-
 Usage:
-	(jump "dialogId")
-	(jumpNow "dialogId")
-	(jump "dialog to print")
-	(jumpNow "dialog to print")
+	{jump "dialogId"}
+	{jumpNow "dialogId"}
 
 Note: be careful of infinite loops, e.g.
 DLG_infinite_loop
 """
-this will print forever(jump "DLG_infinite_loop")
+this will print forever{jump "DLG_infinite_loop"}
 """
 
 HOW TO USE:
@@ -33,9 +27,7 @@ Copy-paste into a script tag after the bitsy source
 */
 
 import bitsy from 'bitsy';
-import {
-	addDualDialogTag,
-} from './helpers/kitsy-script-toolkit';
+import { addDualDialogTag } from './helpers/kitsy-script-toolkit';
 
 // jump function
 function jump(targetDialog) {
@@ -43,15 +35,8 @@ function jump(targetDialog) {
 		console.warn('Tried to jump to dialog, but no target dialog provided');
 		return;
 	}
-	var dialogStr = bitsy.dialog[targetDialog];
-	var dialogId;
-	if (!dialogStr) {
-		dialogStr = targetDialog;
-	} else {
-		dialogId = targetDialog;
-		dialogStr = dialogStr.src;
-	}
-	bitsy.startDialog(dialogStr, dialogId);
+	bitsy.startSpriteDialog({ dlg: targetDialog });
+	bitsy.dialogBuffer.Continue();
 }
 
 addDualDialogTag('jump', function (parameters) {
