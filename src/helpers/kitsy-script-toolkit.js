35c1fb4179e2d0207d48e1eb8bdf87f5d3f6b014
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-06-13 00:56:36 -0400

    feat: refactor out `kitsy` for external reuse
    BREAKING CHANGE: The core of `kitsy-script-toolkit` has been refactored out to a separate module (available [here](https://github.com/seleb/kitsy)), leaving only the bitsy/bitsy-hacks specific code in this project. This shouldn't affect the behaviour of any individual hacks, but may affect dependent projects due to the change in structure.

--- src/helpers/kitsy-script-toolkit.js
+++ src/helpers/kitsy-script-toolkit.js
@@ -1,167 +1,40 @@
-/**
-
-@file kitsy-script-toolkit
-@summary makes it easier and cleaner to run code before and after Bitsy functions or to inject new code into Bitsy script tags
-@license WTFPL (do WTF you want)
-@requires Bitsy Version: 4.5, 4.6
-@author @mildmojo
-
-@description
-HOW TO USE:
-  import {before, after, inject, addDialogTag, addDeferredDialogTag} from "./helpers/kitsy-script-toolkit";
-
-  before(targetFuncName, beforeFn);
-  after(targetFuncName, afterFn);
-  inject(searchRegex, replaceString);
-  addDialogTag(tagName, dialogFn);
-  addDeferredDialogTag(tagName, dialogFn);
-
-  For more info, see the documentation at:
-  https://github.com/seleb/bitsy-hacks/wiki/Coding-with-kitsy
-*/
 import bitsy from 'bitsy';
-import {
-	unique,
-	inject as utilsInject,
-} from './utils';
+import { kitsy } from 'kitsy';
 
-// Ex: inject(/(names.sprite.set\( name, id \);)/, '$1console.dir(names)');
-export function inject(searchRegex, replaceString) {
-	var kitsy = kitsyInit();
-	if (
-		!kitsy.queuedInjectScripts.some(function (script) {
-			return searchRegex.toString() === script.searchRegex.toString() && replaceString === script.replaceString;
-		})
-	) {
-		kitsy.queuedInjectScripts.push({
-			searchRegex: searchRegex,
-			replaceString: replaceString,
-		});
-	} else {
-		console.warn('Ignored duplicate inject');
-	}
-}
-
-// Ex: before('load_game', function run() { alert('Loading!'); });
-//     before('show_text', function run(text) { return text.toUpperCase(); });
-//     before('show_text', function run(text, done) { done(text.toUpperCase()); });
-export function before(targetFuncName, beforeFn) {
-	var kitsy = kitsyInit();
-	kitsy.queuedBeforeScripts[targetFuncName] = kitsy.queuedBeforeScripts[targetFuncName] || [];
-	kitsy.queuedBeforeScripts[targetFuncName].push(beforeFn);
-}
-
-// Ex: after('load_game', function run() { alert('Loaded!'); });
-export function after(targetFuncName, afterFn) {
-	var kitsy = kitsyInit();
-	kitsy.queuedAfterScripts[targetFuncName] = kitsy.queuedAfterScripts[targetFuncName] || [];
-	kitsy.queuedAfterScripts[targetFuncName].push(afterFn);
-}
-
-function kitsyInit() {
-	// return already-initialized kitsy
-	if (bitsy.kitsy) {
-		return bitsy.kitsy;
-	}
-
-	// Initialize kitsy
-	bitsy.kitsy = {
-		queuedInjectScripts: [],
-		queuedBeforeScripts: {},
-		queuedAfterScripts: {},
-	};
-
+var hooked = kitsy.hooked;
+if (!hooked) {
+	kitsy.hooked = true;
 	var oldStartFunc = bitsy.startExportedGame;
 	bitsy.startExportedGame = function doAllInjections() {
 		// Only do this once.
 		bitsy.startExportedGame = oldStartFunc;
 
-		// Rewrite scripts and hook everything up.
-		doInjects();
-		applyAllHooks();
+		// Rewrite scripts
+		kitsy.applyInjects();
+
+		// recreate the script and dialog objects so that they'll be
+		// referencing the code with injections instead of the original
+		bitsy.scriptModule = new bitsy.Script();
+		bitsy.scriptInterpreter = bitsy.scriptModule.CreateInterpreter();
+
+		bitsy.dialogModule = new bitsy.Dialog();
+		bitsy.dialogRenderer = bitsy.dialogModule.CreateRenderer();
+		bitsy.dialogBuffer = bitsy.dialogModule.CreateBuffer();
+
+		// Hook everything
+		kitsy.applyHooks();
 
 		// Start the game
 		bitsy.startExportedGame.apply(this, arguments);
 	};
-
-	return bitsy.kitsy;
 }
 
-function doInjects() {
-	bitsy.kitsy.queuedInjectScripts.forEach(function (injectScript) {
-		utilsInject(injectScript.searchRegex, injectScript.replaceString);
-	});
-	reinitEngine();
-}
-
-function applyAllHooks() {
-	var allHooks = unique(Object.keys(bitsy.kitsy.queuedBeforeScripts).concat(Object.keys(bitsy.kitsy.queuedAfterScripts)));
-	allHooks.forEach(applyHook);
-}
-
-function applyHook(functionName) {
-	var functionNameSegments = functionName.split('.');
-	var obj = bitsy;
-	while (functionNameSegments.length > 1) {
-		obj = obj[functionNameSegments.shift()];
-	}
-	var lastSegment = functionNameSegments[0];
-	var superFn = obj[lastSegment];
-	var superFnLength = superFn ? superFn.length : 0;
-	var functions = [];
-	// start with befores
-	functions = functions.concat(bitsy.kitsy.queuedBeforeScripts[functionName] || []);
-	// then original
-	if (superFn) {
-		functions.push(superFn);
-	}
-	// then afters
-	functions = functions.concat(bitsy.kitsy.queuedAfterScripts[functionName] || []);
-
-	// overwrite original with one which will call each in order
-	obj[lastSegment] = function () {
-		var returnVal;
-		var args = [].slice.call(arguments);
-		var i = 0;
-
-		function runBefore() {
-			// All outta functions? Finish
-			if (i === functions.length) {
-				return returnVal;
-			}
-
-			// Update args if provided.
-			if (arguments.length > 0) {
-				args = [].slice.call(arguments);
-			}
-
-			if (functions[i].length > superFnLength) {
-				// Assume funcs that accept more args than the original are
-				// async and accept a callback as an additional argument.
-				return functions[i++].apply(this, args.concat(runBefore.bind(this)));
-			}
-			// run synchronously
-			returnVal = functions[i++].apply(this, args);
-			if (returnVal && returnVal.length) {
-				args = returnVal;
-			}
-			return runBefore.apply(this, args);
-		}
-
-		return runBefore.apply(this, arguments);
-	};
-}
-
-function reinitEngine() {
-	// recreate the script and dialog objects so that they'll be
-	// referencing the code with injections instead of the original
-	bitsy.scriptModule = new bitsy.Script();
-	bitsy.scriptInterpreter = bitsy.scriptModule.CreateInterpreter();
-
-	bitsy.dialogModule = new bitsy.Dialog();
-	bitsy.dialogRenderer = bitsy.dialogModule.CreateRenderer();
-	bitsy.dialogBuffer = bitsy.dialogModule.CreateBuffer();
-}
+/** @see kitsy.inject */
+export var inject = kitsy.inject;
+/** @see kitsy.before */
+export var before = kitsy.before;
+/** @see kitsy.after */
+export var after = kitsy.after;
 
 // Rewrite custom functions' parentheses to curly braces for Bitsy's
 // interpreter. Unescape escaped parentheticals, too.
@@ -176,7 +49,6 @@ export function convertDialogTags(input, tag) {
 }
 
 function addDialogFunction(tag, fn) {
-	var kitsy = kitsyInit();
 	kitsy.dialogFunctions = kitsy.dialogFunctions || {};
 	if (kitsy.dialogFunctions[tag]) {
 		console.warn('The dialog function "' + tag + '" already exists.');
