29a372670f351cf94918d7e9c55f481cf3ccb831
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-09-08 00:51:46 -0400

    fix(transparent dialog): dialog background not cleared

--- src/transparent dialog.js
+++ src/transparent dialog.js
@@ -9,37 +9,15 @@
 @description
 Makes the dialog box have a transparent background.
 
-Note: this one's ~pretty hacky~.
-
 HOW TO USE:
 Copy-paste into a script tag after the bitsy source
 */
 import bitsy from 'bitsy';
-import {
-	inject,
-} from './helpers/kitsy-script-toolkit';
+import { after } from './helpers/kitsy-script-toolkit';
 
-bitsy.transparentDialog = {
-	canvas: document.createElement('canvas'),
-};
-bitsy.transparentDialog.context = bitsy.transparentDialog.canvas.getContext('2d');
-var drawOverride = `
-if(context == null) return;
-transparentDialog.canvas.width = textboxInfo.width*scale;
-transparentDialog.canvas.height = textboxInfo.height*scale;
-transparentDialog.context.putImageData(textboxInfo.img, 0, 0);
-if (isCentered) {
-	context.drawImage(transparentDialog.canvas, textboxInfo.left*scale, ((height/2)-(textboxInfo.height/2))*scale);
-} else if (player().y < mapsize/2) {
-	context.drawImage(transparentDialog.canvas, textboxInfo.left*scale, (height-textboxInfo.bottom-textboxInfo.height)*scale);
-}
-else {
-	context.drawImage(transparentDialog.canvas, textboxInfo.left*scale, textboxInfo.top*scale);
-}
-return;`;
-
-// override textbox drawing to use draw image version from above
-inject(/(this\.DrawTextbox = function\(\) {)/, '$1' + drawOverride);
-
-// override textbox clearing pixels to be fully transparent
-inject(/(textboxInfo\.img\.data\[i\+3\]=)255/, '$10');
+after('renderClearInstruction', function (bufferId, buffer, paletteIndex) {
+	// 1 -> text buffer
+	if (bufferId !== 1 || paletteIndex !== bitsy.textBackgroundIndex) return;
+	var bufferContext = buffer.canvas.getContext('2d');
+	bufferContext.clearRect(0, 0, buffer.canvas.width, buffer.canvas.height);
+});
