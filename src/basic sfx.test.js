717439e48ca5b042ff9db4c3de18890bb571a87d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-09-23 02:08:42 -0400

    test(basic sfx): add tests

---
+++ src/basic sfx.test.js
@@ -0,0 +1,46 @@
+import { evaluate, press, start, startDialog } from './test/bitsy';
+
+const silencemp3 =
+	'data:audio/mpeg;base64,//uQxAAAAAAAAAAAAAAAAAAAAAAAWGluZwAAAA8AAAACAAACcQCAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICA//////////////////////////////////////////////////////////////////8AAABQTEFNRTMuMTAwBLkAAAAAAAAAABUgJAaUQQAB4AAAAnFiVpzoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//sQxAADwAABpAAAACAAADSAAAAETEFNRTMuMTAwVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVX/+xDEKYPAAAGkAAAAIAAANIAAAARVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVQ==';
+
+test('basic sfx', async () => {
+	await start({
+		hacks: [
+			[
+				'basic sfx',
+				{
+					walk: { src: silencemp3 },
+					talk: { src: silencemp3 },
+				},
+			],
+		],
+	});
+	await press('ArrowRight'); // skip title dialog
+	await press('ArrowRight'); // complete title dialog
+
+	// adds audio elements
+	expect(await evaluate(() => Array.from(window.document.querySelectorAll('audio')).map(i => i.id))).toEqual(['walk', 'talk']);
+
+	// hack audio elements to test playback
+	await evaluate(() => {
+		function play() {
+			this.dataset.played = parseInt(this.dataset.played || 0, 10) + 1;
+		}
+		Array.from(window.document.querySelectorAll('audio')).forEach(i => {
+			i.play = play;
+		});
+	});
+
+	// test "walk"
+	await press('ArrowRight');
+	expect(await evaluate(() => window.document.querySelector('#walk').dataset.played)).toBe('1');
+	await press('ArrowRight');
+	expect(await evaluate(() => window.document.querySelector('#walk').dataset.played)).toBe('2');
+
+	// test "talk"
+	await startDialog('test{pg}test');
+	await press('ArrowRight');
+	expect(await evaluate(() => window.document.querySelector('#talk').dataset.played)).toBe('1');
+	await press('ArrowRight');
+	expect(await evaluate(() => window.document.querySelector('#talk').dataset.played)).toBe('2');
+});
