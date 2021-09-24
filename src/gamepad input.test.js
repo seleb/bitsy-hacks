260b6f5e4e0db9310be61b632f2ab06b85d69e20
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-09-24 01:10:45 -0400

    test(gamepad input): add tests

---
+++ src/gamepad input.test.js
@@ -0,0 +1,43 @@
+import { evaluate, snapshot, start, startDialog, waitForFrame } from './test/bitsy';
+
+async function gamepadSetDownPressed(pressed) {
+	await evaluate(p => {
+		window.navigator.getGamepads = () => [
+			{
+				index: 0,
+				connected: true,
+				buttons: [
+					{
+						pressed: p,
+					},
+				],
+				axes: [],
+			},
+		];
+	}, pressed);
+}
+
+async function gamepadPressDown() {
+	await gamepadSetDownPressed(true);
+	await waitForFrame();
+	await gamepadSetDownPressed(false);
+}
+
+test('gamepad input', async () => {
+	await start({
+		hacks: ['gamepad input'],
+		title: '',
+	});
+
+	await snapshot();
+	// moves
+	await gamepadPressDown();
+	await snapshot();
+	// skips dialog
+	await startDialog('test');
+	await gamepadPressDown();
+	await snapshot();
+	// completes dialog
+	await gamepadPressDown();
+	await snapshot();
+});
