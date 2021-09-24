bec34ef8ae743c042c80cad2289ec063ef38aa4f
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-09-24 02:46:32 -0400

    test(dialog box transition): add fuzzy test

---
+++ src/dialog box transition.test.js
@@ -0,0 +1,19 @@
+import { press, snapshot, start, startDialog } from './test/bitsy';
+
+test('dialog box transition', async () => {
+	await start({
+		title: '',
+		hacks: ['dialog box transition'],
+	});
+	const fuzzyMatch = { failureThreshold: 0.04, failureThresholdType: 'percent' };
+	await snapshot();
+	await startDialog('test');
+	await press('ArrowRight'); // complete dialog page
+	await snapshot(fuzzyMatch);
+	await snapshot(fuzzyMatch);
+	await snapshot(fuzzyMatch);
+	await press('ArrowRight'); // end dialog
+	await snapshot(fuzzyMatch);
+	await snapshot(fuzzyMatch);
+	await snapshot(fuzzyMatch);
+});
