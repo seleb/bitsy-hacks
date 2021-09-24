b4ddae84fec65323a8498e27040ba5cbe734e3a8
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-09-24 02:11:56 -0400

    fix(dialog box transition): dialog transition hack not working

--- src/dialog box transition.js
+++ src/dialog box transition.js
@@ -21,44 +21,49 @@ export var hackOptions = {
 };
 
 var drawOverride = `
-if(context == null) return;
-		if (isCentered) {
-			context.putImageData(textboxInfo.img, textboxInfo.left*scale, ((height/2)-(textboxInfo.height/2))*scale);
-			this.onExit = ((height/2)-(textboxInfo.height/2))*scale === ((height/2)-(textboxInfo.height/2))*scale
-		}
-		else if (player().y < mapsize/2) {
-			easingDialog(textboxInfo, ${hackOptions.easing}, 
-				!this.onClose ? (height-textboxInfo.bottom-textboxInfo.height)*scale
-				: (height+textboxInfo.bottom+textboxInfo.height)*scale
-				 ) 
-			this.onExit = this.onClose && textboxInfo.y >= (height+textboxInfo.height)*scale
-		}
-		else {
-			easingDialog(textboxInfo, ${hackOptions.easing}, !this.onClose ? textboxInfo.top*scale : 
-				-textboxInfo.top-textboxInfo.height*scale) 
-			 this.onExit = this.onClose && textboxInfo.y <= -textboxInfo.height*scale
-		}
+if (isCentered) {
+	bitsyDrawBegin(0);
+	bitsyDrawTextbox(textboxInfo.left, ((height / 2) - (textboxInfo.height / 2)));
+	bitsyDrawEnd();
+	this.onExit = ((height/2)-(textboxInfo.height/2)) === ((height/2)-(textboxInfo.height/2))
+}
+else if (player().y < mapsize/2) {
+	easingDialog(textboxInfo, ${hackOptions.easing}, !this.onClose
+		? (height-textboxInfo.bottom-textboxInfo.height)
+		: (height+textboxInfo.bottom+textboxInfo.height)
+	);
+	this.onExit = this.onClose && textboxInfo.y >= height+textboxInfo.height;
+}
+else {
+	easingDialog(textboxInfo, ${hackOptions.easing}, !this.onClose
+		? textboxInfo.top
+		: -textboxInfo.top-textboxInfo.height
+	);
+	this.onExit = this.onClose && textboxInfo.y <= -textboxInfo.height;
+}
 return;`;
 
 var functionEasing = `
 	function easingDialog(tbox, easing, targetY) {
 		var vy = (targetY - tbox.y) * easing;
 		tbox.y += vy;
-		context.putImageData(tbox.img,tbox.left*scale,tbox.y);
+		bitsyDrawBegin(0);
+		bitsyDrawTextbox(tbox.left, tbox.y);
+		bitsyDrawEnd();
 	}
 	this.onClose = false;
 	this.onExit = false;
 `;
 
 inject(/(this\.DrawTextbox\(\))/, '$1\nif(this.onExit && this.onClose){dialogBuffer.EndDialog()}');
-inject(/(this\.EndDialog\(\))/, 'dialogRenderer.onClose=true');
+inject(/(\/\/end dialog mode\s+this\.EndDialog\(\))/m, 'dialogRenderer.onClose=true');
 inject(/(var DialogRenderer = function\(\) {)/, `$1${functionEasing}`);
 inject(/(var textboxInfo = {)/, '$1y:0,');
 inject(
 	/(this\.Reset = function\(\) {)/,
 	`$1 this.onClose=false;
 		this.onExit=false;
-		textboxInfo.y = player().y < mapsize/2 ? (height+textboxInfo.bottom+textboxInfo.height)*scale : -(textboxInfo.height) * scale;`
+		textboxInfo.y = player().y < mapsize/2 ? (height+textboxInfo.bottom+textboxInfo.height) : -(textboxInfo.height);`
 );
 
 inject(/(this\.DrawTextbox = function\(\) {)/, `$1${drawOverride}`);
