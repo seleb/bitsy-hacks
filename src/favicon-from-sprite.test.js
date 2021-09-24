9a07c5b6644364ff0480bfa0064de063e5b3fa15
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-09-24 01:44:34 -0400

    test(favicon-from-sprite): add tests

---
+++ src/favicon-from-sprite.test.js
@@ -0,0 +1,46 @@
+import { evaluate, snapshot, start } from './test/bitsy';
+
+function fullscreenFavicon() {
+	return evaluate(() => {
+		const url = document.querySelector('#favicon').href;
+		const image = new Image();
+		image.src = url;
+		document.body.appendChild(image);
+		image.style.position = 'fixed';
+		image.style.top = '0';
+		image.style.left = '0';
+		image.style.width = '100%';
+		image.style.height = '100%';
+		image.style.imageRendering = 'pixelated';
+		window.canvas.style.display = 'none';
+	});
+}
+
+test('favicon-from-sprite', async () => {
+	await start({
+		hacks: ['favicon-from-sprite'],
+	});
+	await fullscreenFavicon();
+	await snapshot();
+});
+
+test('options', async () => {
+	await start({
+		hacks: [
+			[
+				'favicon-from-sprite',
+				{
+					SPRITE_NAME: 'a',
+					PALETTE_ID: 0,
+					BG_COLOR_NUM: 1,
+					FG_COLOR_NUM: 0,
+					PIXEL_PADDING: 4,
+					ROUNDED_CORNERS: false,
+					FRAME_DELAY: 400,
+				},
+			],
+		],
+	});
+	await fullscreenFavicon();
+	await snapshot();
+});
