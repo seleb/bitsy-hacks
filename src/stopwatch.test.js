a8b22141cb80718efacce995c7cf757120e8298f
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-09-24 00:54:48 -0400

    test(stopwatch): add tests

---
+++ src/stopwatch.test.js
@@ -0,0 +1,49 @@
+import { evaluate, press, snapshot, start, startDialog } from './test/bitsy';
+
+test('stopwatch', async () => {
+	await start({
+		hacks: ['stopwatch'],
+		title: '',
+	});
+
+	// basic (1:05)
+	await evaluate(() => {
+		window.Date.now = () => 0;
+	});
+	await startDialog('{startWatch "timer"}');
+	await evaluate(() => {
+		window.Date.now = () => 1000 * 65;
+	});
+	await startDialog('{stopWatch "timer"}');
+	await startDialog('{sayWatch "timer"}');
+	await press('ArrowRight');
+	await snapshot();
+	await press('ArrowRight');
+
+	// resume (1:05 + 5s = 1:10)
+	await evaluate(() => {
+		window.Date.now = () => 0;
+	});
+	await startDialog('{resumeWatch "timer"}');
+	await evaluate(() => {
+		window.Date.now = () => 1000 * 5;
+	});
+	await startDialog('{stopWatch "timer"}');
+	await startDialog('{sayWatch "timer"}');
+	await press('ArrowRight');
+	await snapshot();
+	await press('ArrowRight');
+
+	// restart (0:05)
+	await evaluate(() => {
+		window.Date.now = () => 0;
+	});
+	await startDialog('{startWatch "timer"}');
+	await evaluate(() => {
+		window.Date.now = () => 1000 * 5;
+	});
+	await startDialog('{stopWatch "timer"}');
+	await startDialog('{sayWatch "timer"}');
+	await press('ArrowRight');
+	await snapshot();
+});
