d49e371ed112a6981477d84072788dd092c2e20e
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-09-25 03:29:38 -0400

    test(dialog audio): add tests

---
+++ src/dialog audio.test.js
@@ -0,0 +1,25 @@
+import { delay, evaluate, press, start, walkToCat } from './test/bitsy';
+
+test('dialog audio', async () => {
+	await start({
+		hacks: [
+			[
+				'dialog audio',
+				{
+					onLetter: function (dialogChar) {
+						window.characters = window.characters || [];
+						window.characters.push(dialogChar);
+					},
+				},
+			],
+		],
+	});
+	await walkToCat();
+	await press('ArrowRight'); // talk to cat
+	await delay(1500);
+	expect(await evaluate(() => window.characters.map(i => i.char).join(''))).toMatchInlineSnapshot(`"Write your game's title hereI'm a cat"`);
+	await press('ArrowRight'); // end dialog
+	await press('ArrowRight'); // talk to cat
+	await press('ArrowRight'); // complete dialog
+	expect(await evaluate(() => window.characters.map(i => i.char).join(''))).toMatchInlineSnapshot(`"Write your game's title hereI'm a catI'm a cat"`);
+});
