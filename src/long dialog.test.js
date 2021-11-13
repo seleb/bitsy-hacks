0ab3fa40c28e31c4d3346ee10c1729270d33c4ba
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-11-13 03:28:03 -0500

    test long dialog dynamic size change
    Co-Authored-By: ducklingsmith <39173556+ducklingsmith@users.noreply.github.com>

--- src/long dialog.test.js
+++ src/long dialog.test.js
@@ -17,3 +17,23 @@ test('long dialog', async () => {
 	await press('ArrowRight'); // complete dialog page
 	await snapshot();
 });
+
+test('dialog tag', async () => {
+	await start({
+		hacks: ['long dialog'],
+		catDialog: `"""
+{sequence
+  - {textboxsizeNow "4,6"}Test
+  - {textboxsizeNow "2,2"}Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet neque quis volutpat interdum. Aenean blandit volutpat maximus. Nam lectus purus, convallis nec finibus sit amet, malesuada eu nisl.
+}
+"""`,
+	});
+	await walkToCat();
+	await press('ArrowRight'); // talk to cat
+	await press('ArrowRight'); // complete dialog page
+	await snapshot();
+	await press('ArrowRight'); // end dialog
+	await press('ArrowRight'); // talk to cat
+	await press('ArrowRight'); // complete dialog page
+	await snapshot();
+});
