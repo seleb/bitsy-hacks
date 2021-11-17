4dd06cee644aa575c9a146bfe2e69fa631942828
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-11-17 00:41:08 -0500

    chore: autoformat

--- TopLevelOptionsPlugin.mjs
+++ TopLevelOptionsPlugin.mjs
@@ -5,22 +5,20 @@ export default function () {
 	return {
 		renderChunk(code) {
 			// workaround for issues introduced by https://github.com/rollup/rollup/pull/3710/files
-			code = code
-				.replace(/bitsy__default\["default"\]/g, 'bitsy')
-				.replace(/var bitsy__default = /, 'bitsy = bitsy || ');
-			
+			code = code.replace(/bitsy__default\["default"\]/g, 'bitsy').replace(/var bitsy__default = /, 'bitsy = bitsy || ');
+
 			// move options
 			const pattern = /^var\s+hackOptions.*?\s?=\s?{[\s\S]*?^};$/gm;
 			const matches = code.match(pattern);
 			if (!matches) {
 				return {
-					code: code
+					code: code,
 				};
 			}
 			const header = matches[matches.length - 1];
 			return {
-				code: code.replace(header, '').replace("'use strict';", `'use strict';\n${header}`)
+				code: code.replace(header, '').replace("'use strict';", `'use strict';\n${header}`),
 			};
-		}
+		},
 	};
 }
