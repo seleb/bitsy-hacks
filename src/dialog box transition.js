417ccdf34a387934cb41511c0f5e59a9fb254f1d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-07-09 18:09:39 -0400

    fix(dialog box transition): total rewrite for v8
    BREAKING CHANGE: the `dialog box transition` hack is very different in v8, and requires you to specify and a duration and an easing function instead of just a single easing constant. see its hack options for more info

--- src/dialog box transition.js
+++ src/dialog box transition.js
@@ -12,56 +12,50 @@ HOW TO USE:
 1. Copy-paste this script into a script tag after the bitsy source.
 */
 
-import { inject } from './helpers/kitsy-script-toolkit';
+import bitsy from 'bitsy';
+import { after, before } from './helpers/kitsy-script-toolkit';
 
 export var hackOptions = {
-	easing: 0.025, //  easing speed
+	// easing function applied to the transition
+	// the provided parameter is the time in 0-1,
+	// and the expected return value is a value in 0-1
+	// that indicates how much to lerp between the
+	// screen edge and the final position
+	//
+	// the default is a quadratic ease out,
+	// see https://gist.github.com/gre/1650294 for more examples
+	ease: function (t) {
+		return t * (2 - t);
+	},
+	// how long the transition lasts
+	duration: 500,
 };
 
-var drawOverride = `
-if (isCentered) {
-	bitsyDrawBegin(0);
-	bitsyDrawTextbox(textboxInfo.left, ((height / 2) - (textboxInfo.height / 2)));
-	bitsyDrawEnd();
-	this.onExit = ((height/2)-(textboxInfo.height/2)) === ((height/2)-(textboxInfo.height/2))
+function lerp(from, to, by) {
+	return from + (to - from) * by;
 }
-else if (player().y < mapsize/2) {
-	easingDialog(textboxInfo, ${hackOptions.easing}, !this.onClose
-		? height-textboxInfo.bottom-textboxInfo.height
-		: height+textboxInfo.height
-	);
-	this.onExit = this.onClose && textboxInfo.y >= height;
-}
-else {
-	easingDialog(textboxInfo, ${hackOptions.easing}, !this.onClose
-		? textboxInfo.top
-		: -textboxInfo.top-textboxInfo.height
-	);
-	this.onExit = this.onClose && textboxInfo.y <= -textboxInfo.height;
-}
-return;`;
 
-var functionEasing = `
-	function easingDialog(tbox, easing, targetY) {
-		var vy = (targetY - tbox.y) * easing;
-		tbox.y += vy;
-		bitsyDrawBegin(0);
-		bitsyDrawTextbox(tbox.left, tbox.y);
-		bitsyDrawEnd();
+var start;
+var height = 0;
+
+// always redraw the full room when sprites are moving
+before('drawRoom', function (room, args) {
+	args.redrawAll = true;
+	return [room, args];
+});
+
+var edge = 0;
+before('bitsy.textbox', function (visible, x, y, w, h) {
+	height = h || height;
+	edge = y < bitsy.height / 2 ? -height : bitsy.height;
+});
+before('bitsy._graphics.drawImage', function (id, x, y) {
+	if (id !== bitsy.bitsy.TEXTBOX) return undefined;
+	if (!start) {
+		start = bitsy.prevTime;
 	}
-	this.onClose = false;
-	this.onExit = false;
-`;
-
-inject(/(this\.DrawTextbox\(\))/, '$1\nif(this.onExit && this.onClose){dialogBuffer.EndDialog()}');
-inject(/(\/\/end dialog mode\s+this\.EndDialog\(\))/m, 'dialogRenderer.onClose=true');
-inject(/(var DialogRenderer = function\(\) {)/, `$1${functionEasing}`);
-inject(/(var textboxInfo = {)/, '$1y:0,');
-inject(
-	/(this\.Reset = function\(\) {)/,
-	`$1 this.onClose=false;
-		this.onExit=false;
-		textboxInfo.y = player().y < mapsize/2 ? height : -(textboxInfo.height);`
-);
-
-inject(/(this\.DrawTextbox = function\(\) {)/, `$1${drawOverride}`);
+	return [id, x, lerp(edge, y, hackOptions.ease(Math.min(Math.max(0, (bitsy.prevTime - start) / hackOptions.duration), 1)))];
+});
+after('onExitDialog', function () {
+	start = undefined;
+});
