3bbdc2e0f2ccbd089799a4bbe8e87545cad0be8a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-07-09 17:24:26 -0400

    test(dialog audio): wait for dialog blip

--- src/dialog audio.test.js
+++ src/dialog audio.test.js
@@ -1,4 +1,4 @@
-import { delay, evaluate, press, start, walkToCat } from './test/bitsy';
+import { delay, evaluate, press, start, waitForBlip, walkToCat } from './test/bitsy';
 
 test('dialog audio', async () => {
 	await start({
@@ -16,10 +16,12 @@ test('dialog audio', async () => {
 	});
 	await walkToCat();
 	await press('ArrowRight'); // talk to cat
+	await waitForBlip();
 	await delay(1500);
 	expect(await evaluate(() => window.characters.map(i => i.char).join(''))).toMatchInlineSnapshot(`"Write your game's title hereI'm a cat"`);
 	await press('ArrowRight'); // end dialog
 	await press('ArrowRight'); // talk to cat
+	await waitForBlip();
 	await press('ArrowRight'); // complete dialog
 	expect(await evaluate(() => window.characters.map(i => i.char).join(''))).toMatchInlineSnapshot(`"Write your game's title hereI'm a catI'm a cat"`);
 });
