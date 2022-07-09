ac72f0194fbb9f012f78ee3227cebf4a09928b1f
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-07-09 15:13:44 -0400

    test(backdrop): wait for dialog blip

--- src/backdrops.test.js
+++ src/backdrops.test.js
@@ -1,4 +1,4 @@
-import { press, snapshot, start, walkToCat } from './test/bitsy';
+import { press, snapshot, start, waitForBlip, walkToCat } from './test/bitsy';
 
 test('by room', async () => {
 	await start({
@@ -21,6 +21,7 @@ test('by room', async () => {
 	await walkToCat();
 	await snapshot();
 	await press('ArrowRight'); // talk to cat
+	await waitForBlip();
 	await press('ArrowRight'); // complete dialog page
 	await snapshot();
 });
