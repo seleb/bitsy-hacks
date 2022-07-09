863651351cd3ad65d8fe2becf56a43cf3b38d7b1
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-07-09 18:10:05 -0400

    test(dialog box transition): rewrite for new hack options

--- src/dialog box transition.test.js
+++ src/dialog box transition.test.js
@@ -1,19 +1,30 @@
-import { press, snapshot, start, startDialog } from './test/bitsy';
+import { delay, press, snapshot, start, startDialog } from './test/bitsy';
 
 test('dialog box transition', async () => {
 	await start({
 		title: '',
-		hacks: ['dialog box transition'],
+		hacks: [
+			[
+				'dialog box transition',
+				{
+					ease: function (t) {
+						return Math.floor(t * 4) / 4;
+					},
+					duration: 4000,
+				},
+			],
+		],
 	});
 	const fuzzyMatch = { failureThreshold: 0.1, failureThresholdType: 'percent' };
 	await snapshot();
 	await startDialog('test');
-	await press('ArrowRight'); // complete dialog page
+	await delay(1000);
 	await snapshot(fuzzyMatch);
+	await delay(1000);
 	await snapshot(fuzzyMatch);
+	await delay(1000);
 	await snapshot(fuzzyMatch);
+	await delay(1000);
 	await press('ArrowRight'); // end dialog
-	await snapshot(fuzzyMatch);
-	await snapshot(fuzzyMatch);
-	await snapshot(fuzzyMatch);
+	await snapshot();
 });
